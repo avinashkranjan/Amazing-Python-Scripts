8145306400ab04d46925a50b061930a49cc51558
Avinash Kumar Ranjan <ranjan.avinash@hotmail.com>
2020-07-17 22:37:05 +0530

    Screenshot Using CLI

--- Screenshot/screenshot.py
+++ Screenshot/screenshot.py
@@ -3,7 +3,7 @@ import pyautogui
 
 def screenshot():
     name = int(round(time.time() * 1000))
-    name = 'E:/Python-Programs/PythonProjects/Screenshot/ScreenshotsData/{}.png'.format(name)       # To name the file
+    name = '{}.png'.format(name)       # To name the file
     time.sleep(5)           #  Time Wait Before Taking Screenshot 
     img = pyautogui.screenshot(name)
     img.show()              # To Show the Screenshot After Being Taken
