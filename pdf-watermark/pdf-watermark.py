26f104c53e7190cb53962de5f3407ab547764d6e
Madiha Mallick <70858557+madihamallick@users.noreply.github.com>
2020-09-13 20:11:38 +0530

    Update pdf-watermark.py

--- pdf-watermark/pdf-watermark.py
+++ pdf-watermark/pdf-watermark.py
@@ -1,17 +1,18 @@
 from PyPDF2 import PdfFileWriter
 from PyPDF2 import PdfFileReader
 
+input_File = input("Enter File Name You want to add Watermark too[Include .pdf extension]: ")
+watermark = input("Enter Watermark File Name [Include .pdf extension]: ")
 
-ori_pdf = PdfFileReader('inputfile.pdf')
-mark = PdfFileReader('watermark2.pdf')
-
+ori_pdf = PdfFileReader(input_File)
+mark = PdfFileReader(watermark)
 
 watermark = mark.getPage(0)
 original_pdf = ori_pdf.getPage(0)
 
 ori_page_count = ori_pdf.getNumPages()
 
-output = open('finalfile.pdf','wb')
+output = open('watermarked.pdf','wb')
 final = PdfFileWriter()
 
 for i in range(ori_page_count):
