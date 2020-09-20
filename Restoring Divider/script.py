66832d2ade5a96c27f41c6486abb0eb10689ba30
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2020-09-20 11:24:35 +0530

    Updated script.py added comments

--- Restoring Divider/script.py
+++ Restoring Divider/script.py
@@ -5,7 +5,13 @@ def main():
     Q=bin(Q).replace("0b", "")
     M=bin(M).replace("0b", "")
     size=0
-    
+    """
+    This part makes the initialisation of required for the Restoring Division to occur.
+    Which includes:
+    1) Setting an extra to M compared to A
+    2) Filling up A with zeroes
+    3) Setting the size
+    """
     if len(M)==len(Q):
             M="0"+M
     else:
@@ -20,6 +26,9 @@ def main():
     for i in range (0,len(M),1):
         A="0"+A
     size=len(M)
+    """
+    The Calculation and Line by Line Display begins from here
+    """
     A="0"+A
     M="0"+M
     M2=twos_complement(M)
@@ -30,21 +39,30 @@ def main():
     print("M2=",M2)
     printer="A\t\tQ\t\tSize\t\tSteps"
     print(printer)
-    printer=A+"\t\t"+Q+"\t\t"+str(size)+"\t\tInitialization"
+    printer=A+"\t\t"+Q+"\t\t"+str(size)+"\t\tInitialization" #Printing the Initialisation step
     print(printer)
-    #print("A=",A)
-    #print("Q=",Q)
-    #print("size=",size)
-    
+    """
+    The division will be taking place until the size of the Divisor becomes zero
+    """
     for i in range(size,0,-1):
-        
+        """
+        Left Shift Operation
+        """
         A=A[1:len(A)]+Q[0]
         Q=Q[1:len(Q)]
         printer=A+"\t\t"+Q+"\t\t"+str(size)+"\t\tLeft Shift"
         print(printer)
+        
+        """
+        Subtraction
+        """
         A=add(A,M2)
         printer=A+"\t\t"+Q+"\t\t"+str(size)+"\t\tSubtraction"
         print(printer)
+        
+        """
+        Bit Checking and AAddition if required
+        """
         if A[0]=='0':
             Q=Q+"1"
         else:
@@ -52,6 +70,10 @@ def main():
             A=add(A,M)
         printer=A+"\t\t"+Q+"\t\t"+str(size)+"\t\tBit Checking"
         print(printer)
+        
+        """
+        Decreasing Size
+        """
         size=size-1
         printer=A+"\t\t"+Q+"\t\t"+str(size)
         print(printer)
@@ -59,11 +81,17 @@ def main():
 def twos_complement(n):
     a=""
     c=""
+    """
+    Performing 1's Complement by changing all zeroes to one
+    """
     for i in range(0,len(n)):
         if n[i]=='1':
             a=a+"0"
         else:
             a=a+"1"
+    """
+    Performing 2's complement by adding 1 to the 1's complement
+    """
     d=""
     for i in range (0,len(a)-1):
         d=d+"0"
@@ -72,6 +100,9 @@ def twos_complement(n):
     return c
 
 def add(x,y):
+    """
+    Binary Adddition bing carried out
+    """
     carry=""
     result=""
     carry="0"
@@ -106,9 +137,4 @@ def add(x,y):
                 result="0"+result
                 carry='1'
     return result
-
-def left_shift(A,Q):
-    A=A[1:len(A)]+Q[0]
-    Q=Q[1:len(Q)]
-
 main()
