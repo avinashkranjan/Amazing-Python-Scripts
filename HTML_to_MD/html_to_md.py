b6e3dbe593862c72b2b0a82d3d9bda65d7a432ad
ragreenburg <ragreenburg@gmail.com>
2020-10-05 10:59:23 -0700

    HTML to MD file

---
+++ HTML_to_MD/html_to_md.py
@@ -0,0 +1,14 @@
+import html2md
+
+path = "Path/To/Your/HTML"
+
+def html_to_md():
+    html = open(path, 'r').read()
+    md = html2md.convert(html)
+    md_path = "Path/Where/You/Want/To/Save_MD.file"
+    with open(md_path, 'w') as mrd:
+        mrd.write(md)
+        mrd.close()
+    md_save_str = f"You have successfully written the HTML to a MD file and it is saved at {md_path}"
+    print(md_save_str)
+html_to_md()
