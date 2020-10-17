6501dfac8dc785df502d11537f5bca12d08b7387
Jatin Saini <itsjatin135@gmail.com>
2020-10-17 22:53:32 +0530

    Update Instagram Profile Pic Downloader.py

--- Instagram Profile Pic Downloader/Instagram Profile Pic Downloader.py
+++ Instagram Profile Pic Downloader/Instagram Profile Pic Downloader.py
@@ -1,7 +1,16 @@
-#use the following command to install instaloader
-#pip3 install instaloader
+# It will download image in hd
 
-import instaloader
-username = str(input('Enter username: '))
-x = instaloader.Instaloader()
-print(x.download_profile(username, profile_pic_only=True))
+import requests
+from bs4 import BeautifulSoup as soup
+
+
+if __name__ == '__main__':
+  uname = str(input("Enter the username : "))
+  new = requests.get('https://www.instadp.com/fullsize/{}'.format(uname))
+  imgsoup = soup(new.content,'lxml')
+  imglink = imgsoup.find('img',{'class':'picture'})['src']
+  imgfind = requests.get(imglink)
+  photo = open('photo.png','wb')
+  photo.write(imgfind.content)
+  photo.close()
+  
