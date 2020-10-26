0149e73860e62b482645c586b3fd0f2492583f3f
avinashkranjan <ranjan.avinash@hotmail.com>
2020-10-26 22:13:27 +0530

    gh-pages

--- Text-To-Speech/text-to-speech.py
+++
@@ -1,8 +0,0 @@
-import pyttsx3
-
-data = input("Enter Text You Want To Covert: \n")
-
-engine = pyttsx3.init()
-engine.say(data)
-engine.runAndWait()
-
--- URLShortener/url-shortener.py
+++
@@ -1,5 +0,0 @@
-import pyshorteners
-
-url = input("Enter URL: ")
-
-print ("URL after Shortening : ", pyshorteners.Shortener().tinyurl.short(url))
