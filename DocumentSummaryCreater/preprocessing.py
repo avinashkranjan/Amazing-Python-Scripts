5a714595cd9d65c6c8840708235bfcccb2ac1dd4
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-11-10 10:07:17 +0900

    [update]comment to the code

--- DocumentSummaryCreater/preprocessing.py
+++ DocumentSummaryCreater/preprocessing.py
@@ -2,15 +2,18 @@ import spacy
 import neologdn
 
 class EnglishCorpus:
+    # Preparation of morphological analyzer
     def __init__(self):
         self.nlp = spacy.load("en_core_web_sm")
 
+    # Pre-processing of line breaks and special characters
     def preprocessing(self, text:str) -> str:
         text = text.replace("\n", "")
         text = neologdn.normalize(text)        
 
         return text
 
+    # Divide sentences into sentences while retaining the results of morphological analysis
     def make_sentence_list(self, sentences:str) -> list:
         doc = self.nlp(sentences)
         self.ginza_sents_object = doc.sents
@@ -18,6 +21,7 @@ class EnglishCorpus:
 
         return sentence_list
 
+    # Put a space between words
     def make_corpus(self) -> list:
         corpus = []
         for s in self.ginza_sents_object:
