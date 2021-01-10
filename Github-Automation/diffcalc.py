7e5f40513deda7b0f179721cc96b268ef0036d21
Antriksh Misri <54466356+antrikshmisri@users.noreply.github.com>
2021-01-10 20:42:47 +0530

    fixed diff calculation

--- Github-Automation/diffcalc.py
+++ Github-Automation/diffcalc.py
@@ -2,7 +2,7 @@ import difflib
 
 def calcDiff(firstFile , secondFile):
     diff = difflib.ndiff(firstFile , secondFile)
-    delta = ''.join(x[2:] for x in diff if x.startswith('- '))
+    delta = ''.join(x[2:] for x in diff if x.startswith('+ '))
     deltaNoSpace = delta.split('\n')
     print('CHANGED LINES ARE:-\n-----------------')
     for ele in deltaNoSpace:
