27189e42829735d71e81232a813f6fdb11a4b947
RohanTrix <52482556+RohanTrix@users.noreply.github.com>
2021-01-20 11:36:24 +0000

    Time delay between autocommits are based on user input now.

--- Github-Automation/auto_commit_push.py
+++ Github-Automation/auto_commit_push.py
@@ -4,11 +4,11 @@ n = int(input("Enter Auto-commit and Push time delay(in sec): "))
 while(True):
     if os.system('git diff --exit-code')==0:
         print(0)
-        time.sleep(60)
+        time.sleep(n)
         f = open('helloworld.txt','w')
         f.write(str(time.time()))
         f.close()
         continue
     os.system('git add-commit -m "Committed changes"')
     os.system('git push -u origin master')
-    time.sleep(60)
+    time.sleep(n)
