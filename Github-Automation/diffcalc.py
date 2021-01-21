13ea7a7579ce50b0cc7ffe5c817343c236b1334b
antrikshmisri <antrikshmisri@gmail.com>
2021-01-21 13:01:55 +0530

    fix: nested-files,diff calc,add to gitignore

--- Github-Automation/diffcalc.py
+++ Github-Automation/diffcalc.py
@@ -1,10 +1,21 @@
+
 import difflib
+
+def getMaxSpaces(file):
+    max = float('-inf')
+    for ele in file:
+        ele = ele.strip()
+        if(len(ele) > max):
+            max = len(ele)
+    return max
 
 def calcDiff(firstFile , secondFile):
     diff = difflib.ndiff(firstFile , secondFile)
-    delta = ''.join(x[2:] for x in diff if x.startswith('+ '))
-    deltaNoSpace = delta.split('\n')
-    print('CHANGED LINES ARE:-\n-----------------')
-    for ele in deltaNoSpace:
-        print(ele.strip())
-    print('-----------------\n')
+    deltainit = ''.join(x[2:] for x in diff if x.startswith('+ '))
+    deltainit = deltainit.split('\n')
+    maxspacesinit = getMaxSpaces(deltainit)
+    print('CHANGED LINES ARE:-\n' , '-' * maxspacesinit)
+    for ele in deltainit:
+        print(str(ele.strip()) , ' ' * (maxspacesinit - len(ele.strip())), '+|')
+    print('' , '-' * maxspacesinit)
+    
