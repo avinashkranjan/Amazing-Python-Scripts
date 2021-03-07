27d5938e607631e8daae329b18b0731203a32ae3
Muthu Annamalai Venkatachalam <muthuannamalai2002@gmail.com>
2021-03-07 22:41:38 +0530

    Create Voice_control_Module.py

---
+++ whatsapp-bot/Voice_control_Module.py
@@ -0,0 +1,34 @@
+# required stuff for voice control
+# importing the required libraries
+import smtplib
+from difflib import SequenceMatcher
+
+import pyttsx3
+import speech_recognition as sr
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+    print(audio)
+
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("accessing...")
+        query = r.recognize_google(audio, language="en-in")
+        print(f"User said: {query}\n")
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")
+        return "None"
+      
