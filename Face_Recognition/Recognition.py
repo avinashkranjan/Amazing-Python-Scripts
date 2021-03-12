77df39ea63a25bf30777e202e391ba643c4a44a4
aakriti1318 <aakritiaggarwal2k@gmail.com>
2021-03-12 21:28:24 +0530

    Recognition

--- Face_Recognition/Recognition.py
+++ Face_Recognition/Recognition.py
@@ -4,7 +4,7 @@ import os
 haar_cascade = cv.CascadeClassifier('haar_face.xml')
 
 people = []
-DIR = r'images' ## DIR - path of the folder containing images
+DIR = r'images'  # DIR - path of the folder containing images
 for i in os.listdir(DIR):
     people.append(i)
 
@@ -18,15 +18,15 @@ face_recognizer.read('face_trained.yml')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Person', gray)
 
-faces_rect = haar_cascade.detectMultiScale(gray,1.1,4)
+faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
 
-for (x,y,w,h) in faces_rect:
-    faces = gray[y:y+h,x:x+h]
-    
+for (x, y, w, h) in faces_rect:
+    faces = gray[y:y+h, x:x+h]
+
     label, confidence = face_recognizer.predict(faces)
-    cv.putText(img,str(people[label]), (20,20), cv.FONT_HERSHEY_COMPLEX, 1.0,
-    (0,255,0), thickness = 2)
-    cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness = 2)
+    cv.putText(img, str(people[label]), (20, 20), cv.FONT_HERSHEY_COMPLEX, 1.0,
+               (0, 255, 0), thickness=2)
+    cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
 
 cv.imshow('detected face', img)
 
--- Face_Recognition/face_train.py
+++ Face_Recognition/face_train.py
@@ -1,59 +1,62 @@
 # Face Recognition with OpenCV built in classifier
-# Aim: This script recognizes the face in an image using OpenCV's built-in recognizer. The vast application of this project is 
-# recognizing the images in the running videos. 
-# Feature: Easy and understandable code 
+# Aim: This script recognizes the face in an image using OpenCV's built-in recognizer. The vast application of this project is
+# recognizing the images in the running videos.
+# Feature: Easy and understandable code
 
 import os
 import cv2 as cv
 import numpy as np
 
 people = []
-DIR = r'images' # DIR - path of the folder containing images
-for i in os.listdir(DIR): # Will append the names of the person in the people list 
+DIR = r'images'  # DIR - path of the folder containing images
+for i in os.listdir(DIR):  # Will append the names of the person in the people list
     people.append(i)
 
 haar_cascade = cv.CascadeClassifier('haar_face.xml')
 
-# For every face in the list the corresponding labels to the features are designated. 
-features = [] 
+# For every face in the list the corresponding labels to the features are designated.
+features = []
 labels = []
+
 
 def train_face():
     for person in people:
-        path = os.path.join(DIR,person)
+        path = os.path.join(DIR, person)
         label = people.index(person)
 
         for img in os.listdir(path):
-            img_path = os.path.join(path,img)
+            img_path = os.path.join(path, img)
 
             img_arr = cv.imread(img_path)
-            gray = cv.cvtColor(img_arr,cv.COLOR_BGR2GRAY)
+            gray = cv.cvtColor(img_arr, cv.COLOR_BGR2GRAY)
 
-            face_rect = haar_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors=4)
+            face_rect = haar_cascade.detectMultiScale(
+                gray, scaleFactor=1.1, minNeighbors=4)
 
-            for (x,y,w,h) in face_rect:
-                face_roi = gray[y:y+h , x:x+w]
+            for (x, y, w, h) in face_rect:
+                face_roi = gray[y:y+h, x:x+w]
                 features.append(face_roi)
                 labels.append(label)
+
 
 train_face()
 
 # Feature and labels numpy array
-features = np.array(features,dtype='object') 
+features = np.array(features, dtype='object')
 labels = np.array(labels)
 
 # Train our recognizer
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 
 # Trained on features and the labels list
-face_recognizer.train(features,labels)
+face_recognizer.train(features, labels)
 
-# Just like haar cascade yml file, we can save this file in yml format so that 
+# Just like haar cascade yml file, we can save this file in yml format so that
 # we can easily access this trained model
 face_recognizer.save('face_trained.yml')
 
-####  save the features and labels into the numpy file 
+# save the features and labels into the numpy file
 # simply remove the comments of the below 2 line
 #
-# np.save('features.npy', features) 
+# np.save('features.npy', features)
 # np.save('labels.npy',labels)
