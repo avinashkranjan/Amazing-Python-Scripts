c5fb11a1d2fba716be733bd35c80e4e389ccd1bf
Ankur Gupta <31045221+ankurg132@users.noreply.github.com>
2021-03-13 00:34:37 +0530

    Added comments

--- extracting-emails-from-website/emails-from-website.py
+++ extracting-emails-from-website/emails-from-website.py
@@ -2,11 +2,10 @@ import re
 import requests
 url = input("Enter Url: ")
 text = requests.get(url).text
-x = re.findall(r"[a-zA-Z]+@{1}[a-zA-Z]+[.]{1}[a-zA-Z]+", text)
-val=""
-for i in x:
-    val+=i+"\n"
-filename= input("Enter the file Name you want: ")
+x = re.findall(r"[a-zA-Z]+@{1}[a-zA-Z]+[.]{1}[a-zA-Z]+", text)   #This is a regex query which search for the particular email format.
+for i in x: 
+    val+=i+"\n"                                                  #this adds all the email data found in val variable  
+filename= input("Enter the file Name you want: ")                #You can use any file name you want for your output file
 with open(f"{filename}.txt","w") as file:
     file.write(val)
     print("Your File has been Saved")
