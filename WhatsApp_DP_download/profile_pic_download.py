b3efcfc80950f729a22a81f78e4d56114fc63005
Sudip Ghosh <60208804+sudipg4112001@users.noreply.github.com>
2021-03-13 09:37:18 +0530

    Update profile_pic_download.py

--- WhatsApp_DP_download/profile_pic_download.py
+++ WhatsApp_DP_download/profile_pic_download.py
@@ -1,4 +1,4 @@
-# region IMPORTS
+
 from pathlib import Path
 
 from wplay.utils import browser_config
@@ -7,12 +7,12 @@ from wplay.utils import target_select
 from wplay.utils.helpers import profile_photos_path
 from wplay.utils.helpers import whatsapp_selectors_dict
 from wplay.utils.Logger import Logger
-# endregion
 
 
-# region LOGGER
+
+
 __logger = Logger(Path(__file__).name)
-# endregion
+
 
 
 async def get_profile_picture(target):
@@ -27,7 +27,7 @@ async def get_profile_picture(target):
             await target_search.search_and_select_target_without_new_chat_button(page, target)
     else:
         target = await target_select.manual_select_target(page)
-    # Getting Profile picture url
+
     selector = '#main > header > div > div > img'
     await page.waitForSelector(selector, timeout=2000)
     image_url = await page.evaluate(f'document.querySelector("{selector}").getAttribute("src")')
