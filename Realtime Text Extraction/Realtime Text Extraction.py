9f660aa81064fd2bb98b25a4a671c4f3e51e72f2
Shubham Gupta <57499043+ShubhamGupta577@users.noreply.github.com>
2021-03-14 00:20:29 +0530

    Required Changes have been made.

--- Realtime Text Extraction/Realtime Text Extraction.py
+++ Realtime Text Extraction/Realtime Text Extraction.py
@@ -1,7 +1,3 @@
-'''
-For using this code your machine had already installed OpenCV, pytesseract and PIL.
-For capturing a image press 'SPACE' when web-cam is on.
-'''
 #importing required libraries
 
 import cv2
@@ -9,18 +5,19 @@ import pytesseract
 from PIL import Image
 
 #Put the path where you stored the tesseract.exe file in your machine
-pytesseract.pytesseract.tesseract_cmd = r"C:\Users\91769\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+pytesseract_file=input("Enter the path where you stored the tesseract.exe file\n")
+pytesseract.pytesseract.tesseract_cmd = pytesseract_file
 
 
 cam = cv2.VideoCapture(0)	                                        #Starting your webcam
 
 while True:
     ret, img = cam.read()	#Capturing the image
-    #img= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)                    Can convert colour image to grayscale
+    gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)              #Can convert colour image to grayscale
     
     cv2.imshow("Original",img)
     #Removing noise from the image
-    ret, thresh = cv2.threshold(img,1010, 200, cv2.THRESH_OTSU, cv2.THRESH_BINARY)
+    ret, thresh = cv2.threshold(gray,1010, 200, cv2.THRESH_OTSU, cv2.THRESH_BINARY)
     cv2.imshow("After removing noise", thresh)
 
     if not ret:
@@ -37,11 +34,12 @@ while True:
         #For Space key
         print("Image saved")
         #Put the path where you want to store the captured image in your machine
-        path=r'C:\Users\91769\Desktop\images'+'\img.jpg'
+        path=input("Enter the path where you want to store the image\n ")
+        path=path+'\img.jpg'
         cv2.imwrite(path, thresh)
         break
 
-src= Image.open(r"C:\Users\91769\Desktop\images\img.jpg")      
+src= Image.open(path)      
 
 text= pytesseract.image_to_string(src)                            #Extracting the text from image
 print(text)
