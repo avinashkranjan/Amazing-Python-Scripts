a6b8a9e00866f309ae4ba2bc831c96aa361f954b
saaalik <mdsaaalik@gmail.com>
2021-03-13 16:24:40 +0530

    Debugged Email Validation Script #297

--- Email-Validator/email_verification.py
+++ Email-Validator/email_verification.py
@@ -7,45 +7,51 @@ import re
 def check_syntax(email):
     regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
     if(re.search(regex,email)):
-        pass
+        print("Check 1 (Syntax) Passed")
     else:  
-        print("Invalid Email! Bad Syntax")
+        print("Check 1 FAILED! Bad Syntax, Invalid Email!")
         exit()
 
 #SECOND CHECK
-def check_dns(email):
-    domain = email.split("@")[-1]
+def check_dns(email,domain):
     try:
         records = resolver.resolve(domain, 'MX')
         mxRecord = str(records[0].exchange)
+        print("Check 2 (DNS -",mxRecord+") Passed")
         return mxRecord
     except:
-        print("Invalid Email! The domain",domain,"does not exist")
+        print("Check 2 FAILED! The domain",domain,"does not exist, Invalid Email!")
         exit()
 
 #THIRD CHECK
-def check_response(email, mxRecord):
-    # Get local server hostname
-    host = socket.gethostname()
+def check_response(email, domain, mxRecord):
+    try:
+        # Get local server hostname
+        host = socket.gethostname()
 
-    # SMTP lib setup (use debug level for full output)
-    server = smtplib.SMTP()
-    server.set_debuglevel(0)
+        # SMTP lib setup (use debug level for full output)
+        server = smtplib.SMTP()
+        server.set_debuglevel(0)
 
-    # SMTP Conversation
-    server.connect(mxRecord)
-    server.helo(host)
-    server.mail(email)
-    code, message = server.rcpt(str(email))
-    server.quit()
+        # SMTP Conversation
+        server.connect(mxRecord)
+        server.helo(host)
+        server.mail(email)
+        code, message = server.rcpt(str(email))
+        server.quit()
 
-    # Assume 250 as Success
-    if code == 250:
-        print("The email address",email,"is VALID")
-    else:
-        print("Invalid Email!")
+        # Assume 250 as Success
+        if code == 250:
+            print("Check 3 (SMTP response) Passed")
+            print(email,"is a VALID email address!")
+        else:
+            print("Check 3 FAILED! The user",email.split("@")[0],"does not exist, Invalid Email!")
+    except socket.error as socketerror:
+        print("Check 3 HALTED! The domain",domain,", either does not have an SMTP or have restricted access through external scripts")
 
-email = input()
+#email = input()
+email = "mdsaaalik@gmail.com"
+domain = email.split("@")[-1]
 check_syntax(email)#CHECK1
-mxRecord = check_dns(email)#CHECK2
-check_response(email,mxRecord)#CHECK3
+mxRecord = check_dns(email,domain)#CHECK2
+check_response(email,domain,mxRecord)#CHECK3
