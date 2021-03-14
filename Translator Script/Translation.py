fd245d2c898a31bca74af7ddee71b1ba79d12332
NEERAJ ADITYANANTH POLAMPALLI <65017645+NEERAJAP2001@users.noreply.github.com>
2021-03-14 14:44:54 +0530

    Update Translation.py

--- Translator Script/Translation.py
+++ Translator Script/Translation.py
@@ -1,27 +1,13 @@
-
-
 # First Step : Go to cmd and type: pip install googletrans==3.1.0a0
-
-
 # Using the Translator Class
 from googletrans import Translator
-
 # Making Object of Translator Class
 translator  = Translator()
-
-
 # Word you want to translate
 Word = input('Enter what you want to translate : ')
-
-
-
 # For further language Codes
 # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
 Code = input('Enter Language Code : ')
-
-
 # Using the translator method to get work done
 translation = translator.translate(Word,dest=Code)
-
-# printing to console
 print(translation.text)
