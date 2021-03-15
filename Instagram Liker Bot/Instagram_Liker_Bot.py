37d869d36e376f6549b3df25e70e8ee9cd0b9513
sayantani11 <ii.sayantani.ii@gmail.com>
2021-03-15 20:55:25 +0530

    Instagram Liker Bot- made changes and fixed bugs

--- Instagram Liker Bot/Instagram_Liker_Bot.py
+++ Instagram Liker Bot/Instagram_Liker_Bot.py
@@ -5,11 +5,11 @@ from getpass import getpass
 import time
 
 chrome=webdriver.Chrome(ChromeDriverManager().install())
-
 chrome.get("https://www.instagram.com")
+time.sleep(10)
 
 username=chrome.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
-username.send_keys("") #enter username
+username.send_keys(input()) #enter username
 password= chrome.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
 pswd=getpass()
 password.send_keys(pswd)
@@ -18,27 +18,30 @@ login_button.click()
 time.sleep(10)
 
 search_bar= chrome.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
-search_bar.send_keys(" ") #enetr the username to be searched
-time.sleep(5)
+search_bar.send_keys(input()) #enter the username to be searched
+time.sleep(7)
 search_bar.send_keys(Keys.ENTER)
 search_bar.send_keys(Keys.ENTER)
 
-post = chrome.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[2]/article/div[1]/div/div[1]/div[1]/a/div/div[2]')
+time.sleep(2)
+
+post = chrome.find_element_by_xpath('/html/body/div[1]/section/main/div/div[2]/article/div[1]/div/div[1]/div[1]/a/div[1]/div[2]')
 post.click()
-like_button= chrome.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[1]/button/div/span/svg/path')
+time.sleep(2)
+like_button= chrome.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[1]/button')
 like_button.click()
-next_button = chrome.find_element_by_xpath('/html/body/div[4]/div[1]/div/div/a')
+next_button = chrome.find_element_by_xpath('/html/body/div[5]/div[1]/div/div/a')
 next_button.click()
-time.sleep(3)
+time.sleep(2)
 
 while True:
     try:
-        like_button= chrome.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[1]/button/div/span/svg/path')
+        like_button= chrome.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[1]/button')
         like_button.click()
-        next_button = chrome.find_element_by_xpath('/html/body/div[4]/div[1]/div/div/a')
+        next_button = chrome.find_element_by_xpath('/html/body/div[5]/div[1]/div/div/a[2]')
         next_button.click()
         time.sleep(5)
     except:
-        close_button = chrome.find_element_by_xpath('/html/body/div[4]/div[3]/button/div/svg')
+        close_button = chrome.find_element_by_xpath('/html/body/div[5]/div[3]/button')
         close_button.click()
         break
