ac50ea1012d75a4891b0c40bdf878f6787b08184
Aliya Rahmani <65673692+aliya-rahmani@users.noreply.github.com>
2021-03-15 20:46:46 +0530

    Update cartooneffect.py

--- Cartooning Image/cartooneffect.py
+++ Cartooning Image/cartooneffect.py
@@ -1,43 +1,22 @@
-import cv2
-
-num_down = 2 # number of downsampling steps
-num_bilateral = 7 # number of bilateral filtering steps
-
-img_rgb = cv2.imread("myCat.jpg")
-
-# downsample image using Gaussian pyramid
-img_color = img_rgb
-for _ in range(num_down):
-   img_color = cv2.pyrDown(img_color)
-
-# repeatedly apply small bilateral filter instead of
-# applying one large filter
-for _ in range(num_bilateral):
-img_color = cv2.bilateralFilter(img_color, d=9, sigmaColor=9, sigmaSpace=7)
-
-# upsample image to original size
-for _ in range(num_down):
-   img_color = cv2.pyrUp(img_color)
-
-#Use median filter to reduce noise
-# convert to grayscale and apply median blur
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
-img_blur = cv2.medianBlur(img_gray, 7)
-
-
-#Use adaptive thresholding to create an edge mask
-# detect and enhance edges
-img_edge = cv2.adaptiveThreshold(img_blur, 255,
-   cv2.ADAPTIVE_THRESH_MEAN_C,
-   cv2.THRESH_BINARY,
-   blockSize=9,
-   C=2)
-
-
-# Combine color image with edge mask & display picture
-# convert back to color, bit-AND with color image
-img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
-img_cartoon = cv2.bitwise_and(img_color, img_edge)
-
-# display
-cv2.imshow("myCat_cartoon", img_cartoon)
+import cv2 
+import numpy as np 
+  
+# reading image  
+img = cv2.imread("koala.jpeg") 
+   
+# Edges 
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+gray = cv2.medianBlur(gray, 5) 
+edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,  
+                                         cv2.THRESH_BINARY, 9, 9) 
+   
+# Cartoonization 
+color = cv2.bilateralFilter(img, 9, 250, 250) 
+cartoon = cv2.bitwise_and(color, color, mask=edges) 
+   
+   
+cv2.imshow("Image", img) 
+cv2.imshow("edges", edges) 
+cv2.imshow("Cartoon", cartoon) 
+cv2.waitKey(0) 
+cv2.destroyAllWindows() 
