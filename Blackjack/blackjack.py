156ded376a1a059e3845275e43685c16d76c9202
Meha Bhalodiya <73488906+mehabhalodiya@users.noreply.github.com>
2021-03-16 15:12:15 +0530

    Update blackjack.py

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -2,17 +2,15 @@
 
 import random
 
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8,
-          'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
-
 playing = True
 
 
 # CLASSES
 
 class Card:  # Creates all the cards
+          
+    suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+    ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 
     def __init__(self, suit, rank):
         self.suit = suit
@@ -45,6 +43,9 @@ class Deck:  # creates a deck of cards
 
 
 class Hand:   # show all the cards that the dealer and player have
+          
+    values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8,
+          'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
 
     def __init__(self):
         self.cards = []
