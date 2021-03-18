1882b72c323cb7bf5f6a257f38434879050c0b94
LakhanKumawat <55774240+Lakhankumawat@users.noreply.github.com>
2021-03-18 13:13:19 +0530

    Update ImageCompressing.py

--- Image Processing/ImageCompressing.py
+++ Image Processing/ImageCompressing.py
@@ -3,9 +3,9 @@
 # Python Imaging Library PIL
 from PIL import Image as ig
 
-#open the image from directory , in my case its in same directory
-img=ig.open("Home/Pictures/default.jpg") # Here we have to provide complete address of image from file which contains your py file 
-
+# Here we have to provide complete address of image from file which contains your py file 
+filePath=input("Enter the path of your Image : ")
+img=ig.open(filePath)
 #Apply the compressing
 img.save("Compressed.jpg",optimize=True,quality=30)
 
