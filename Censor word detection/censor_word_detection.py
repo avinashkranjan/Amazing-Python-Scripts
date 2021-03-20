4b3d3ad7c424a14ddb59c74f385a01b5439e5123
jeelnathani <37493874+jeelnathani@users.noreply.github.com>
2021-03-20 17:48:07 +0530

    Update censor_word_detection.py

--- Censor word detection/censor_word_detection.py
+++ Censor word detection/censor_word_detection.py
@@ -2,8 +2,6 @@ from better_profanity import profanity
 
 def detect(data):
     try:
-        '''profanity.censor(data) will print **** '''
-        '''profanity.censor(data,'#') will print #### if censor word it detected; else it will print data you have entered '''
         output=profanity.censor(data,'#')
         print(output)
     except Exception as e:
