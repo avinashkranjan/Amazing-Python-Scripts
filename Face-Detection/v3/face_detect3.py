984b30a56055e763af6840102f764c011410d8ec
Avishake Maji <56298861+Avishake007@users.noreply.github.com>
2021-03-21 11:11:34 +0530

    Create face_detect3.py

---
+++ Face-Detection/v3/face_detect3.py
@@ -0,0 +1,34 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface.xml")
+
+
+def detect():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        _, img = cap.read()
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        face = face_cascade.detectMultiScale(gray, 1.1, 4)
+        a=str(len(face))
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        if int(a)==1:
+            cv2.putText(img,a+"face detected",(15,350), font, 2,(255,0,0),2,cv2.LINE_AA)
+        else:
+            cv2.putText(img,a+"faces detected",(15,350), font, 2,(255,0,0),2,cv2.LINE_AA)
+
+        for (x, y, w, h) in face:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cv2.imshow("Face Detect", img)
+
+        if cv2.waitKey(1) &  0xFF == ord('q'):# Press q to quit
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+detect()
