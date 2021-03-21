611e3fecb5e8f0a18b9d6ea04e2978eb22378312
Avishake Maji <56298861+Avishake007@users.noreply.github.com>
2021-03-21 11:24:39 +0530

    Update face_detectv3.py

--- Face-Detection/v3/face_detectv3.py
+++ Face-Detection/v3/face_detectv3.py
@@ -15,9 +15,9 @@ def detect():
         a=len(face)
         font = cv2.FONT_HERSHEY_SIMPLEX
         if a<=1:
-            cv2.putText(img,str(a)+"face detected",(15,350), font, 2,(255,0,0),2,cv2.LINE_AA)
+            cv2.putText(img,f"{a} face detected",(15,350), font, 2,(255,0,0),2,cv2.LINE_AA)
         else:
-            cv2.putText(img,str(a)+"faces detected",(15,350), font, 2,(255,0,0),2,cv2.LINE_AA)
+            cv2.putText(img,f"{a} faces detected",(15,350), font, 2,(255,0,0),2,cv2.LINE_AA)
         
         for (x, y, w, h) in face:
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
