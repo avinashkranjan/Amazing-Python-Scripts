5fc81f60410e85df8ee76067e0caf2cee7f6afbc
Avishake Maji <56298861+Avishake007@users.noreply.github.com>
2021-03-21 11:14:01 +0530

    Update face-detect.py

--- Face-Detection/face-detect.py
+++ Face-Detection/face-detect.py
@@ -12,23 +12,16 @@ def detect():
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         face = face_cascade.detectMultiScale(gray, 1.1, 4)
-        a=str(len(face))
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        if int(a)==1:
-            cv2.putText(img,a+"face detected",(15,350), font, 2,(255,0,0),2,cv2.LINE_AA)
-        else:
-            cv2.putText(img,a+"faces detected",(15,350), font, 2,(255,0,0),2,cv2.LINE_AA)
 
         for (x, y, w, h) in face:
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         cv2.imshow("Face Detect", img)
 
-        if cv2.waitKey(1) &  0xFF == ord('q'):# Press q to quit
+        if cv2.waitKey(1) == 27:
             break
 
     cap.release()
-    cv2.destroyAllWindows()
 
 
 detect()
