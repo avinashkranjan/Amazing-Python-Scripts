3510986e466b7ba6c9708ff270f739d74250b77f
Muthu Annamalai Venkatachalam <muthuannamalai2002@gmail.com>
2021-03-22 18:39:38 +0530

    Update script.py

--- PDF2text/script.py
+++ PDF2text/script.py
@@ -6,8 +6,8 @@ pdf_path = input("Enter the path of the pdf file : ")
 
 assert os.path.exists(pdf_path), "this pdf file doesn't exist"
 
-with open(pdf_path, 'rb') as f_r:
-    pdf_pages = pdftotext.PDF(f_r)
+with open(f"./PDF2text/pages/{i}.txt", "w") as f:
+    f.write(page)
 
 for i, page in enumerate(pdf_pages):
     print('Page {}'.format(i))
