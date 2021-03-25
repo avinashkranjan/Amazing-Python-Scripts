89b45e23a100ff7fcbc27fdfabdf76a7e94ef175
Amit kumar mishra <akm43300@gmail.com>
2021-03-25 12:38:58 +0530

    PDF2Text

---
+++ PDF2Text/script.py
@@ -0,0 +1,14 @@
+import PyPDF2
+
+pdf = input("Enter the path of PDF file")
+n = int(input("Enter number of pages"))
+st=""
+page = PyPDF2.PdfFileReader(pdf) 
+for i in range(1,n+1):
+    
+    st += page.getPage(i).extractText()
+
+with open('./PDF2Text/pages/text.txt','w') as f:
+    f.write(st)
+
+    
