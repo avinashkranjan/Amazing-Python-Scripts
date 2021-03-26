1a9f0644133633e0b1bdb8e94a0662ccd5ae55e4
Pankaj Biradar <pankaj28988@gmail.com>
2021-03-26 17:16:50 +0530

    Added Battery Notification Script

---
+++ Battery-Notification/Battery-Notification.py
@@ -0,0 +1,17 @@
+import psutil
+from plyer import notification
+import time
+#From psutil we import sensors battery class which gives us battery percentage
+while(True):
+    battery = psutil.sensors_battery()
+    percent = battery.percent
+    notification.notify(
+        title = "Battery Percentage",
+        message = str(percent) + "% Battery Remaining",
+        timeout = 10
+        )
+     #After every 60 minutes it will show the battery percentage via a notification   
+    time.sleep(60*60)
+
+    
+    continue
