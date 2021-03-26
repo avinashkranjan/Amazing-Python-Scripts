51e0fde20e5c479222130e7c8c5a0f8bb1b13be4
Anupreeta Das <42911603+Anupreetadas@users.noreply.github.com>
2021-03-26 13:46:27 +0530

    Update alarm_clock.py

--- Alarm-Clock/alarm_clock.py
+++ Alarm-Clock/alarm_clock.py
@@ -1,149 +1,62 @@
-import tkinter as tk
-from tkinter import ttk
-from datetime import datetime
+from tkinter import *
+import datetime
+import time
 import pygame
+import threading
+import sys
+from threading import Thread
 
-#Initializing the wav alarm file we want it to play when the alarm goes 
 pygame.mixer.init(42050, -16, 2, 2048)
-alarm_sound = pygame.mixer.Sound("./Alarm-Clock/alarm.wav")
-#Setting the initial global values
-start_printed = False
-stop_printed = True
-done = False
-finished = False
-stop_clicked = False
+alarm_sound = pygame.mixer.Sound("alarm.wav")
 
-class AlarmApp(tk.Tk):
-    def __init__(self):
-        tk.Tk.__init__(self)
-        #Title of the window set to 'Alarm Clock'
-        self.title("Alarm Clock")
-        #Set up all of the drop-down lists
-        self.hr = tk.IntVar(self)
-        self.min = tk.IntVar(self)
-        self.ampm = tk.StringVar(self)
-        #Set the initial values of each drop-down list
-        self.hr.set('12')
-        self.min.set("00")
-        self.ampm.set("AM")
-        #Create the list of values for drop down list 
-        hours = []
-        minutes = []
-        ampmlist = ["AM","PM"]
-        #Hours go from 1 to 12
-        for x in range(1,13):
-            hours.append(x)
-        #Minutes go from 0 to 59
-        for y in range(0,60):
-            minutes.append("%02d" % (y,))
-            #Placing all of the list into the respective drop-down list
-        self.popmenuhours = tk.OptionMenu(self,self.hr, *hours)
-        self.popmenuminutes = tk.OptionMenu(self,self.min, *minutes)
-        self.popmenuAMPM = tk.OptionMenu(self, self.ampm, *ampmlist)
-        #Placing our drop-down lists on the page as well as one label
-        self.popmenuhours.pack(side = "left")
-        self.thing = tk.Label(text = ":").pack(side = "left")
-        self.popmenuminutes.pack(side = "left")
-        self.popmenuAMPM.pack(side = "left")
-        #Setting up all the buttons on the right hand side of the window.
-        #Command refers to which function it will run once it's clicked
-        #State refers to whether it is clickable or not at the current state.
-        self.alarmbutton = tk.Button(self, text="Set Alarm", command=self.start_clock)
-        self.cancelbutton = tk.Button(self, text="Cancel Alarm", command=self.stop_clock, state = "disabled")
-        self.stopalarmbutton = tk.Button(self, text = "Stop Alarm", command=self.stop_audio, state = "disabled")
-        #Packing all the buttons into the page
-        self.alarmbutton.pack()
-        self.cancelbutton.pack()
-        self.stopalarmbutton.pack()
+def Threading():
+    t1=Thread(target=actual_time) 
+    t1.start()
+ 
+def alarm(set_alarm_timer):
+    while True:
+        time.sleep(1)
+        current_time = datetime.datetime.now()
+        now = current_time.strftime("%H:%M:%S")
+        date = current_time.strftime("%d/%m/%Y")
+        print(now)
+        if now == set_alarm_timer:
+            print("Time to Wake up")
+            pygame.mixer.Sound.play(alarm_sound, loops = -1)
+            time.sleep(30)
+            break
+            
+        
+def actual_time():
+    set_alarm_timer = f"{hour.get()}:{mins.get()}:{sec.get()}"
+    alarm(set_alarm_timer)
+    
 
-    def start_clock(self):
+clock = Tk()
+def on_closing():
+    pygame.mixer.Sound.stop(alarm_sound)
+    clock.destroy()
+    sys.exit()
 
-        global done, start_printed, stop_printed, stop_clicked
-        #Done refers to whether either the time has been reached or if the user has cancelled. I.e: Loop is done.
-        if done == False:
-            #Cancel button is now active so user can decide at any point to cancel the alarm
-            self.cancelbutton.config(state = "active")
-            #Alarm button is now disabled since an alarm has currently already been set
-            self.alarmbutton.config(state = "disabled")
-            #On the first run of the loop, let the user know that an alarm has been set for their desired time
-            if start_printed == False:
-                #Print this notification for the user in the terminal
-                print("Alarm set for {}:{}{}".format(self.hr.get(), "%02d" % (self.min.get()),self.ampm.get()))
-                start_printed = True
-                stop_printed = False
-           
-            if self.ampm.get() == "AM":
-                if self.hr.get() in range(1,12):
-                    hour_value = self.hr.get()
-                else:
-                    hour_value = self.hr.get() - 12
-            if self.ampm.get() == "PM":
-                if self.hr.get() in range(1,12):
-                    hour_value = self.hr.get() +12
-                else:
-                    hour_value = self.hr.get()
-            #Now we call the Alarm function with the information that the user has entered to check whether we have reached the alarm time
-            self.Alarm("%02d" % (hour_value,), "%02d" % (self.min.get()))
-        #If user has clicked the cancel alarm button, we reset everything
-        if stop_clicked == True:
-            done = False
-            start_printed = False
-            stop_clicked = False
+clock.protocol("WM_DELETE_WINDOW", on_closing) 
+clock.geometry("400x200")
+Label(clock, text= "Enter time in 24 hour format!", fg="red",bg="black",font="Arial").place(x=60,y=120)
+Label(clock,text = "Hour  Min   Sec",font=60).place(x = 110)
+Label(clock,text = "When to wake you up",fg="blue",relief = "solid",font=("Helevetica",7,"bold")).place(x=0, y=29)
+ 
+# The Variables we require to set the alarm(initialization):
+hour = StringVar()
+mins = StringVar()
+sec = StringVar()
+ 
+#Time required to set the alarm clock:
+Entry(clock,textvariable = hour,bg = "white",width = 15).place(x=110,y=30)
+Entry(clock,textvariable = mins,bg = "white",width = 15).place(x=150,y=30)
+Entry(clock,textvariable = sec,bg = "white",width = 15).place(x=200,y=30)
 
-    def stop_clock(self):
-        global done, stop_clicked
-        #Let the user know that the alarm has been cancelled by printing it in the terminal
-        print("Alarm set for {}:{}{} has been cancelled".format(self.hr.get(), "%02d" % (self.min.get()),self.ampm.get()))
-        #Cancel button has now been clicked
-        stop_clicked = True
-        #Now done with the current alarm/loop
-        done = True
-        #Buttons reset to what they were originally
-        self.cancelbutton.config(state = "disabled")
-        self.alarmbutton.config(state = "active")
-
-    def stop_audio(self):
-        #Use PyGame to stop the audio since button has been clicked
-        pygame.mixer.Sound.stop(alarm_sound)
-        #Stop alarm button disabled and alarm button active, essentially reseting everything
-        self.stopalarmbutton.config(state = "disabled")
-        self.alarmbutton.config(state = "active")
-
-
-
-    def Alarm(self,myhour,myminute):
-        global done, start_printed, finished
-        #If we are still not done, we follow this statement
-        if done == False:
-            #We convert the information into strings (To match DateTime)
-            myhour,myminute = str(myhour),str(myminute)
-            #Next, we extract the data of the current time from DateTime and take the information we want (hour and minute)
-            a = str(datetime.now())
-            b = a.split(" ")[1].split(":")
-            hour = b[0]
-            minute = b[1]
-            #Now, if the alarm time matches the current time, we follow his statement. Alarm is going to go off!
-            if hour == myhour and minute == myminute:
-                #Using pygame to play audio, loops = -1 refers to an infinite loop
-                pygame.mixer.Sound.play(alarm_sound, loops = -1)
-                print("Alarm is ringing!")
-                #We are now done
-                done = True
-                #Also finished
-                finished = True
-                #Now we change back the state of the cancel button to disabled, and the state of the alarm stop to active
-                #This is so the user can stop the alarm, since it will infinitely loop
-                self.cancelbutton.config(state = "disabled")
-                self.stopalarmbutton.config(state = "active")
-
-            else:
-                #If it is still not the set time, recursively loop back to the start_clock function
-                self.after(1000, self.start_clock)
-            done = False
-        #If it is finished, which we are when the alarm goes off, we reset everything
-        if finished == True:
-            start_printed = False
-            finished = False
-
-app = AlarmApp()
-app.mainloop()
+ 
+#To take the time input by user:
+Button(clock,text = "Set Alarm",fg="red",width = 10,command = Threading).place(x =110,y=70)
+ 
+clock.mainloop()
+#Execution of the window.
