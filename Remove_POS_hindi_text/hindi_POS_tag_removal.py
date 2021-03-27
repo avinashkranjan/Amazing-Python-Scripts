f99719f06773f86cc6a1450e3ee359dca8bf529f
Sanya Zaveri <zaverisanya@gmail.com>
2021-03-27 11:29:59 +0530

    Update hindi_POS_tag_removal.py

--- Remove_POS_hindi_text/hindi_POS_tag_removal.py
+++ Remove_POS_hindi_text/hindi_POS_tag_removal.py
@@ -1,8 +1,17 @@
 import string
 import nltk
+import sys
+import os
 
-fp=open(r"C:\Users\ZAVERI SANYA\Desktop\Amazing-Python-Scripts\Remove_POS_hindi_text\\Tagged_Hindi_Corpus.txt",mode="r",encoding="utf-8") #opens the hindi_tagged_corpus.txt file
-fd=open(r"C:\Users\ZAVERI SANYA\Desktop\Amazing-Python-Scripts\Remove_POS_hindi_text\\Only_Hindi.txt",mode="a",encoding="utf-8")
+user_input=input(' Enter file location of your Tagged Hindi Text: ')
+#C:\Users\ZAVERI SANYA\Desktop\Amazing-Python-Scripts\Remove_POS_hindi_text\\Tagged_Hindi_Corpus.txt
+assert os.path.exists(user_input), "I did not find the file at, "+str(user_input)
+fp=open(user_input,mode="r",encoding="utf-8") #opens the hindi_tagged_corpus.txt file
+print("Hooray we found your file!")
+
+user_answer= input (' Enter file location where you wish to get your Only Hindi Text file: ')
+#C:\Users\ZAVERI SANYA\Desktop\Amazing-Python-Scripts\Remove_POS_hindi_text\Only_Hindi.txt
+fd=open(user_answer,mode="a",encoding="utf-8")
 data=fp.read()
 data_token=nltk.tokenize.word_tokenize(data) #data tokenization
 words=[]
@@ -18,5 +27,6 @@ str=""
 for word in words:
     str+=word+" " #it concatenates the words
 fd.write(str) #writes to only_hindi.txt file
+print("Hooray your Only Hindi Text file is ready...Please Check!")
 fp.close()
 fd.close()
