bab120c70be64c0eb45fe04d515c28e2d44f7d5c
LakhanKumawat <55774240+Lakhankumawat@users.noreply.github.com>
2021-03-27 13:27:55 +0530

    Update main.py

--- Gmplot-Track the Route/main.py
+++ Gmplot-Track the Route/main.py
@@ -4,9 +4,10 @@ from gmplot import gmplot
 
 
 gmap = gmplot.GoogleMapPlotter(20.613456, 72.9431185,17) #Plotting data on map
-#gmap.coloricon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
 
-with open('Home/MyPC/Python/LatLong.csv','r') as f:  #give the address of csv file with respect to your folder containing .py file
+Path = input("Enter the csv file path")
+
+with open(Path,'r') as f:  #give the address of csv file with respect to your folder containing .py file
     reader=csv.reader(f)
     k=0
     for row in reader:
