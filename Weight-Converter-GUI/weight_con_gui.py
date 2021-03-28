c874c670e60b25f9b745ac6c106b8fe4f408055f
Meha Bhalodiya <73488906+mehabhalodiya@users.noreply.github.com>
2021-03-28 10:36:20 +0530

    Modified weight_con_gui.py

--- Weight-Converter-GUI/weight_con_gui.py
+++ Weight-Converter-GUI/weight_con_gui.py
@@ -34,12 +34,12 @@ def WeightConv():
         kilograms = float(e1.get())
 
         # insert the output in textboxes correct upto 2 places after decimal
-        t1.insert(END, "%.2f Carat" % (kilograms * 5000))
-        t2.insert(END, "%.2f Gram" % (kilograms * 1000))
-        t3.insert(END, "%.2f Ounce" % (kilograms * 35.274))
-        t4.insert(END, "%.2f Pound" % (kilograms * 2.20462))
-        t5.insert(END, "%.2f Quintal" % (kilograms * 0.01))
-        t6.insert(END, "%.2f Tonne" % (kilograms * 0.001))
+        t1.insert(END, "%.2f" % (kilograms * 5000))
+        t2.insert(END, "%.2f" % (kilograms * 1000))
+        t3.insert(END, "%.2f" % (kilograms * 35.274))
+        t4.insert(END, "%.2f" % (kilograms * 2.20462))
+        t5.insert(END, "%.2f" % (kilograms * 0.01))
+        t6.insert(END, "%.2f" % (kilograms * 0.001))
 
     # if blank or invalid input is given then exception is thrown
     except ValueError:
@@ -89,6 +89,24 @@ t5l5.grid(row = 8, column = 1, columnspan=1)
 
 t6l6 = Label(root, text = "kg to t : ")
 t6l6.grid(row = 9, column = 1, columnspan=1)
+
+t1r1 = Label(root, text = "Carat")
+t1r1.grid(row = 4, column = 4, columnspan=1)
+
+t2r2 = Label(root, text = "Gram")
+t2r2.grid(row = 5, column = 4, columnspan=1)
+
+t3r3 = Label(root, text = "Ounce")
+t3r3.grid(row = 6, column = 4, columnspan=1)
+
+t4r4 = Label(root, text = "Pound")
+t4r4.grid(row = 7, column = 4, columnspan=1)
+
+t5r5 = Label(root, text = "Quintal")
+t5r5.grid(row = 8, column = 4, columnspan=1)
+
+t6r6 = Label(root, text = "Tonne")
+t6r6.grid(row = 9, column = 4, columnspan=1)
 
 # creating textbox and defining grid to show output
 t1 = Text(root, height = 1, width = 20)
