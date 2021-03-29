abf49412241a86d7456d48574ce8df87e7467172
Richa Singh <49644949+ricsin23@users.noreply.github.com>
2021-03-29 20:44:39 +0530

    Update Animated_Robot.py

--- Codes on Turtle Graphics/Animated_Robot.py
+++ Codes on Turtle Graphics/Animated_Robot.py
@@ -2,24 +2,24 @@ import turtle as t
 import time as ti
 
 def rectangle(hor,ver,col):
-    t.pendown()
-    t.pensize(1)
+    t.pendown()    # to create a canvas for drawing
+    t.pensize(1)    # size of pen
     t.color(col)
     t.begin_fill()
     for counter in range(1,3):
         t.forward(hor)
-        t.right(90)
+        t.right(90)     #move 90 degree right
         t.forward(ver)
-        t.right(90)
+        t.right(90)   
     t.end_fill()
     t.penup()
 
 t.penup()
 t.speed('slow')
-t.bgcolor('Dodger blue')
+t.bgcolor('Dodger blue')    # give background color
 
-t.goto(-100,-150)
-rectangle(50,20,'blue')
+t.goto(-100,-150)       # move the turtle to given coordinates
+rectangle(50,20,'blue')    # create a rectangle of given size
 t.goto(-30,-150)
 rectangle(50,20,'blue')
 
