fcc936e3f8fd94fde5b8873b1014fea356bc8af1
Anurag Mukherjee <62845847+anuragmukherjee2001@users.noreply.github.com>
2021-03-31 21:06:00 +0530

    Updated the README file

--- Image Viewing GUI/main.py
+++ Image Viewing GUI/main.py
@@ -7,30 +7,15 @@ root = Tk()
 root.title('Image viewer')
 root.resizable(0, 0)
 
-# opening the images from the image folder
-img1 = Image.open("./Image Viewing GUI/images/img1.jpg")
-img2 = Image.open("./Image Viewing GUI/images/img2.jpg")
-img3 = Image.open("./Image Viewing GUI/images/img3.jpg")
-img4 = Image.open("./Image Viewing GUI/images/img4.jpg")
-img5 = Image.open("./Image Viewing GUI/images/img5.jpg")
+# fetching the images from the folder
+cwd = os.path.dirname(os.path.realpath(__file__))
+img_path = os.path.join(cwd, 'images')
 
-# resizing the images
-resized_img1 = img1.resize((500, 500), Image.ANTIALIAS)
-resized_img2 = img2.resize((500, 500), Image.ANTIALIAS)
-resized_img3 = img3.resize((500, 500), Image.ANTIALIAS)
-resized_img4 = img4.resize((500, 500), Image.ANTIALIAS)
-resized_img5 = img5.resize((500, 500), Image.ANTIALIAS)
+# storing the images in a list
+images = os.listdir(img_path)
+image_list = [Image.open(os.path.join(img_path, image)) for image in images]
 
-new_pic1 = ImageTk.PhotoImage(resized_img1)
-new_pic2 = ImageTk.PhotoImage(resized_img2)
-new_pic3 = ImageTk.PhotoImage(resized_img3)
-new_pic4 = ImageTk.PhotoImage(resized_img4)
-new_pic5 = ImageTk.PhotoImage(resized_img5)
-
-# creating a list of all the images
-image_list = [new_pic1,new_pic2,new_pic3,new_pic4,new_pic5]
-
-my_label = Label(image = new_pic1)
+my_label = Label(image = image_list[0])
 my_label.grid(row = 0, column = 0, columnspan = 3)
 
 # defining a function to show the previous image
