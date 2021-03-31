7c1fb643a5ee5382050f2f965a4288c090a59c15
LAPTOP-SNSEHSAR\ayush <ayushjain2205@gmail.com>
2021-03-31 05:59:17 +0530

    Master password functionality added

--- Password-Manager-GUI/passwords.py
+++ Password-Manager-GUI/passwords.py
@@ -1,7 +1,11 @@
 from tkinter import *
-from tkinter import messagebox
+from tkinter import messagebox,simpledialog
 import sqlite3
 from sqlite3 import Error
+import sys
+
+# Store Master password
+master_password = sys.argv[1]
 
 # Function to connect to the SQL Database
 def sql_connection():
@@ -11,28 +15,29 @@ def sql_connection():
     except Error:
         print(Error)
 
-# Function to create table 
+# Function to create table
 def sql_table(con):
     cursorObj = con.cursor()
-    cursorObj.execute("CREATE TABLE IF NOT EXISTS passwords(website text, username text, pass text)")
+    cursorObj.execute(
+        "CREATE TABLE IF NOT EXISTS passwords(website text, username text, pass text)")
     con.commit()
 
 # Call functions to connect to database and create table
 con = sql_connection()
 sql_table(con)
 
-#Create submit function for database
+# Create submit function for database
 def submit(con):
     cursor = con.cursor()
-    #Insert Into Table
-    if website.get()!="" and username.get()!="" and password.get()!="":
+    # Insert Into Table
+    if website.get() != "" and username.get() != "" and password.get() != "":
         cursor.execute("INSERT INTO passwords VALUES (:website, :username, :password)",
-            {
-                'website': website.get(),
-                'username': username.get(),
-                'password': password.get()
-            }
-        )
+                       {
+                           'website': website.get(),
+                           'username': username.get(),
+                           'password': password.get()
+                       }
+                       )
         con.commit()
         # Message box
         messagebox.showinfo("Info", "Record Added in Database!")
@@ -48,29 +53,36 @@ def submit(con):
 # Create Query Function
 def query(con):
 
-    #set button text
-    query_btn.configure(text="Hide Records", command=hide)
-    
-    cursor = con.cursor()
+    password = simpledialog.askstring("Password","Enter Master Password")
+    if(password == master_password):
+        # set button text
+        query_btn.configure(text="Hide Records", command=hide)
+        cursor = con.cursor()
+        # Query the database
+        cursor.execute("SELECT *, oid FROM passwords")
+        records = cursor.fetchall()
 
-    #Query the database
-    cursor.execute("SELECT *, oid FROM passwords")
-    records = cursor.fetchall()
-    #print(records)
+        p_records = 'ID'.ljust(10)+ 'Website'.ljust(40)+'Username'.ljust(70)+'Password'.ljust(100)+'\n'
 
-    p_records = " Id \t\t Website \t\t Username \t\t Password\n"
-    for record in records:
-            p_records += str(record[3])+ "\t\t" +str(record[0])+  "\t\t" + str(record[1])+  "\t\t" + str(record[2]) + "\n"
-        #print(record)
+        for record in records:
+            single_record = ""
+            single_record += (str(record[3]).ljust(10) +
+                          str(record[0]).ljust(40)+str(record[1]).ljust(70)+str(record[2]).ljust(100))
+            single_record += '\n'
+            # print(single_record) 
+            p_records += single_record
+        query_label['text'] = p_records
+        # Commit changes
+        con.commit()
+        p_records = ""
+        
+    else:
+        messagebox.showinfo("Failed!", "Authentication failed!")
 
-    query_label['text'] = p_records
-    # Commit changes
-    con.commit()
-
-#Create Function to Hide Records
+# Create Function to Hide Records
 def hide():
     query_label['text'] = ""
-    query_btn.configure(text="Show Records", command=lambda:query(con))
+    query_btn.configure(text="Show Records", command=lambda: query(con))
 
 
 root = Tk()
@@ -80,38 +92,39 @@ root.minsize(600, 400)
 root.maxsize(600, 400)
 
 frame = Frame(root, bg="#774A9F", bd=5)
-frame.place(relx=0.50, rely=0.50, relwidth=0.98, relheight=0.45, anchor = "n")
+frame.place(relx=0.50, rely=0.50, relwidth=0.98, relheight=0.45, anchor="n")
 
-#Create Text Boxes
+# Create Text Boxes
 website = Entry(root, width=30)
-website.grid(row=1, column=1, padx=20,pady=5)
+website.grid(row=1, column=1, padx=20, pady=5)
 username = Entry(root, width=30)
-username.grid(row=2, column=1, padx=20,pady=5)
+username.grid(row=2, column=1, padx=20, pady=5)
 password = Entry(root, width=30)
-password.grid(row=3, column=1, padx=20,pady=5)
+password.grid(row=3, column=1, padx=20, pady=5)
 
-#Create Text Box Labels
-website_label = Label(root, text = "Website:")
+# Create Text Box Labels
+website_label = Label(root, text="Website:")
 website_label.grid(row=1, column=0)
-username_label = Label(root, text = " Username:")
+username_label = Label(root, text=" Username:")
 username_label.grid(row=2, column=0)
-password_label = Label(root, text = "Password:")
+password_label = Label(root, text="Password:")
 password_label.grid(row=3, column=0)
 
 
-#Create Buttons
-submit_btn = Button(root, text = "Add Password",command = lambda: submit(con))
-submit_btn.grid(row = 5, column=1, pady=5, padx=15, ipadx=35)
-query_btn = Button(root, text = "Show All",command = lambda: query(con))
+# Create Buttons
+submit_btn = Button(root, text="Add Password", command=lambda: submit(con))
+submit_btn.grid(row=5, column=1, pady=5, padx=15, ipadx=35)
+query_btn = Button(root, text="Show All", command=lambda: query(con))
 query_btn.grid(row=6, column=1, pady=5, padx=5, ipadx=35)
 
-#Create a Label to show stored passwords
+# Create a Label to show stored passwords
 global query_label
 query_label = Label(frame, anchor="nw", justify="left")
 query_label.place(relwidth=1, relheight=1)
 
 def main():
     root.mainloop()
+
 
 if __name__ == '__main__':
     main()
