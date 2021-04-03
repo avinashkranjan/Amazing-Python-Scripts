54cfc572f4ff0e21eae2de5aaeb051f2d1bd2dd5
Amit kumar mishra <akm43300@gmail.com>
2021-04-03 21:31:13 +0530

    Update

--- Imagetospeech/image_to_speech.py
+++ Imagetospeech/image_to_speech.py
@@ -10,21 +10,21 @@ img = input(r"Enter image: ")
 target = Image.open(img)
 text = pytesseract.image_to_string(target, config='')
 
-with open(f"./Imagetospeech/Text/text.txt",'w') as f:
+with open(f"./Imagetospeech/text.txt",'w') as f:
     f.write(text)
 
-file = open(r'./Imagetospeech/Text/text.txt')
+file = open(r'./Imagetospeech/text.txt')
 mytext = file.read().replace("\n"," ")
 language = 'en'
 output = gTTS(text=mytext, lang=language, slow=False)
-output.save('./Imagetospeech/sound/imagetospeech.mp3')
+output.save('./Imagetospeech/imagetospeech.mp3')
 file.close()
-os.system("start ./Imagetospeech/sound/imagetospeech.mp3")
+os.system("start ./Imagetospeech/imagetospeech.mp3")
 
 question = input("Do you want to delete the files (Y/N): ")
 if question=='Y'or question=='y':
-    os.remove('./Imagetospeech/Text/text.txt')
-    os.remove('./Imagetospeech/sound/imagetospeech.mp3')
+    os.remove('./Imagetospeech/text.txt')
+    os.remove('./Imagetospeech/imagetospeech.mp3')
 
 elif question=='N'or question=='n':
     print("Files saved")
