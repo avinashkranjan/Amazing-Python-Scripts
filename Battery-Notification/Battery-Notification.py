54daeb978341c36e8fd85f44162a8fbfc8f56618
pankaj892 <31444506+pankaj892@users.noreply.github.com>
2021-04-06 20:14:37 +0530

    Update Battery-Notification.py

--- Battery-Notification/Battery-Notification.py
+++ Battery-Notification/Battery-Notification.py
@@ -8,10 +8,8 @@ while(True):
     notification.notify(
         title = "Battery Percentage",
         message = str(percent) + "% Battery Remaining",
-        timeout = 10
+        timeout = 5
         )
      #After every 60 minutes it will show the battery percentage via a notification   
-    time.sleep(60*60)
-
-    
+    time.sleep(60)
     continue
