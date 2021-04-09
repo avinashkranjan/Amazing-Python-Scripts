cb892a9b483cd9ae52f0bc2a219b5379f7a3b0e4
Yuvraj Kadale <kadaleyuvraj@gmail.com>
2021-04-09 19:19:10 +0530

    Fix GUI Chat additional files

--- Brick Breaker game/brick_breaker.py
+++ Brick Breaker game/brick_breaker.py
@@ -10,7 +10,7 @@ Window_height = 500
 Window_width = 500
 
 window = pygame.display.set_mode((Window_height,Window_width))
-pygame.display.set_caption('Brickstroy')      #game title
+pygame.display.set_caption('Brickstroy')        #game title
 
 '''
 Defining Bricks colour
@@ -18,11 +18,12 @@ Defining Bricks colour
 O_brick = (255,100,10)
 w_brick = (255,255,255)
 g_brick = (0,255,0)
+black = (0,0,0)
 
 game_rows = 6
 game_coloumns = 6
 clock =pygame.time.Clock()        #clock speed
-frame_rate = 60 
+frame_rate = 60
 
 class bricks():
     
@@ -63,127 +64,64 @@ class bricks():
                 pygame.draw.rect(window, (0,0,0), (brick[0]),1)
 
 class base():
-    def __init__(self):
-        self.height = 20
-        self.width = int(Window_width / game_coloumns) 
-        self.x = int((Window_width / 2) - (self.width / 2))
-        self.y = Window_height - (self.height * 2)
-        self.Rectangle = Rect(self.x, self.y, self.width, self.height)
-        self.speed = 10
-        self.direction = 0 
-
-    def movement(self):  
-        self.direction = 0
-        key = pygame.key.get_pressed()
-        if key[pygame.K_LEFT] and self.Rectangle.left > 0:
-            self.Rectangle.x = self.speed
-            self.direction = -1
-        if key[pygame.K_RIGHT] and self.Rectangle.right < Window_width:
-            self.Rectangle.x = self.speed
-            self.direction = 1
+    def __init__(self,x,y,size_x,size_y,color):
+        self.image = pygame.Surface((size_x,size_y),SRCALPHA,32)
+        self.rect = self.image.get_rect()
+        self.rect.left = x
+        self.rect.top = y  # X,Y coordinates for our base
+        
+        self.image.fill(w_brick)
+        
+        self.movement = [0,0] #pixelsto be moved in (x,y) format
+        self.speed= 8
 
     def draw_base(self):
-        pygame.draw.rect(window, (0,0,255), self.Rectangle)
-        pygame.draw.rect(window, (255,255,255), self.Rectangle,1)
+        pygame.draw.rect(window, (0,0,255), self.rect)
+        pygame.draw.rect(window, (255,255,255), self.rect,1)
 
-class ball():
-    def __init__(self,x,y):
-        self.radius = 10
-        self.x = x - self.radius
-        self.y = y - 50
-        self.rect = Rect(self.x, self.y,  self.radius *2, self.radius*2)
-        self.x_speed = 4
-        self.y_speed = -4
-        self.max_speed = 5
-        self.game_over = 0
+    def update(self):
+        self.rect = self.rect.move(self.movement)
+        self.checkbounds()
 
-    def motion(self):
-
-        collision_threshold = 5
-
-        brick_destroyed = 1  # flag set to 1
-        count_row = 0
-        for row in bricks.brick:
-            count_item = 0
-            for item in row:
-                if self.rect.colliderect(item[0]):
-                    if abs(self.rect.bottom - item[0].top) < collision_threshold and self.y_speed >0:
-                        self.y_speed *= -1
-                    if abs(self.rect.top - item[0].bottom) < collision_threshold and self.y_speed < 0:
-                        self.y_speed *= -1
-                    if abs(self.rect.right - item[0].left) < collision_threshold and self.x_speed >0:
-                        self.x_speed *= -1
-                    if abs(self.rect.left - item[0].right) < collision_threshold and self.x_speed < 0:
-                        self.x_speed *= -1
-                    if bricks.brick[count_row][count_item][1] >1:
-                        bricks.brick[count_row][count_item][1] -= 1
-                    else:
-                        bricks.brick[count_row][count_item][0] > (0,0,0,0)
-
-                if bricks.brick[count_row][count_item][0] != (0,0,0,0):
-                    brick_destroyed = 0   
-                count_item +=1
-            count_row +=1
-        if brick_destroyed == 1:
-            self.game_over = 1
-
-
-        # collision check for game window
-        if self.rect.left < 0 or self.rect.right > Window_width:
-            self.x_speed *=-1
-
-        if self.rect.top < 0:
-            self.y_speed *=-1
-        if self.rect.bottom > Window_height:
-            self.game_over = -1
-
-        # collision check for base
-        if self.rect.colliderect(user_basepad):
-            if abs(self.rect.bottom - user_basepad.rect.top) < collision_threshold and self.y_speed > 0:
-                self.y_speed *=-1
-                self.x_speed *= user_basepad.direction
-                if self.x_speed > self.max_speed:
-                    self.x_speed = self.max_speed
-                elif self.x_speed <0 and self.x_speed < -self.max_speed:
-                    self.x_speed = -self.max_speed
-                
-                else:
-                    self.x_speed *= -1
-
-        self.rect.x == self.x_speed
-        self.rect.y == self.y_speed
-
-        return self.game_over
-
-    def draw(self):
-        pygame.draw.circle(window, (0,0,255), (self.rect.x + self.radius, self.rect.y + self.radius), self.radius)
-        pygame.draw.circle(window, (255,255,255), (self.rect.x + self.radius, self.rect.y + self.radius), self.radius,2)
-
-
-
+    def checkbounds(self):
+        if self.rect.left < 0:
+            self.rect.left = 0
+        
+        if self.rect.right > Window_width:
+            self.rect.right = Window_width
 
 brick = bricks()
 brick.make_brick()
 
-user_basepad = base()
+user_basepad = base(Window_width/2,Window_height - Window_height/10,80,10,w_brick)
 
-ball = ball(user_basepad.x + (user_basepad.width//2), user_basepad.y + user_basepad.height)
+#ball = ball(user_basepad.x + (user_basepad.width//2), user_basepad.y + user_basepad.height)
 
 game= True
 while game:
 
     clock.tick(frame_rate)
-    brick.draw_brick()
+    window.fill(black)
+    user_basepad.update()
     user_basepad.draw_base()
-    user_basepad.movement()
-
-    ball.draw()
-    ball.motion()
+    brick.draw_brick()
+ 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game=False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                user_basepad.movement[0] = -1*user_basepad.speed
+            if event.key == pygame.K_RIGHT:
+                user_basepad.movement[0]= user_basepad.speed
 
-    pygame.display.flip()   # added for any updates that we make to the gaming window to become visible.
- 
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                user_basepad.movement[0] = 0
+
+    
+    pygame.display.update()   # added for any updates that we make to the gaming window to become visible.
+    
 pygame.quit()
