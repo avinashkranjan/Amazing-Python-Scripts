e51926757f234330e5e4fd07a3d3e6e93a43f4a3
Meha Bhalodiya <73488906+mehabhalodiya@users.noreply.github.com>
2021-04-09 22:11:55 +0530

    Added finger counter

---
+++ Finger-Counter/finger_counter.py
@@ -0,0 +1,70 @@
+import cv2
+import time
+import os
+import HandTrackingModule as htm
+
+wCam, hCam = 640, 480
+
+cap = cv2.VideoCapture(1)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+folderPath = "FingerImages"
+myList = os.listdir(folderPath)
+print(myList)
+overlayList = []
+
+for imPath in myList:
+    image = cv2.imread(f'{folderPath}/{imPath}')
+    overlayList.append(image)
+
+print(len(overlayList))
+
+previousTime = 0
+
+detector = htm.handDetector(detectionCon = 0.75)
+
+# 4 for thumb, 8 for index, 12 for middle, 16 for ring, 20 for pinky finger
+tipIds = [4, 8, 12, 16, 20]
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw = False)
+    print(lmList)
+
+    if len(lmList) != 0:
+        fingers = []
+
+        # Thumb
+        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][2]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # Four fingers
+        for id in range(1,5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        totalFingers = fingers.count(1)
+        print(totalFingers)
+
+        h, w, c = overlayList[totalFingers - 1].shape
+        img[0:h, 0:w] = overlayList[totalFingers - 1]
+
+        cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 25)
+
+    currentTime = time.time()
+    fps = 1/(currentTime - previousTime)
+    previousTime = currentTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (400,70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
+
