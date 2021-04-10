6b16b1a94c346da2024ec91475e23d77c5aa7191
pankaj892 <31444506+pankaj892@users.noreply.github.com>
2021-04-10 14:24:41 +0530

    Update Battery-Notification.py

--- Battery-Notification/Battery-Notification.py
+++ Battery-Notification/Battery-Notification.py
@@ -2,14 +2,20 @@ import psutil
 from plyer import notification
 import time
 #From psutil we import sensors battery class which gives us battery percentage
+threshold = int(input('Enter the threshold: '))
 while(True):
     battery = psutil.sensors_battery()
     percent = battery.percent
-    notification.notify(
-        title = "Battery Percentage",
-        message = str(percent) + "% Battery Remaining",
-        timeout = 5
-        )
-     #After every 60 minutes it will show the battery percentage via a notification   
-    time.sleep(60)
+    time.sleep(60*2)
+    battery = psutil.sensors_battery()
+    cur_per = battery.percent
+    change = cur_per - percent
+    diff = abs(change)
+    #We calculate the change in the battery and show notification if battery level increases or decreases
+    if(diff >= threshold):
+        notification.notify(
+            title = "Battery Percentage",
+            message = str(percent) + "% Battery Remaining",
+            timeout = 5
+        )   
     continue
