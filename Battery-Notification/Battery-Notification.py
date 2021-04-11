58733869ffb60fe59ac90f15eb03c102719e077d
pankaj892 <31444506+pankaj892@users.noreply.github.com>
2021-04-11 12:44:39 +0530

    Update Battery-Notification.py

--- Battery-Notification/Battery-Notification.py
+++ Battery-Notification/Battery-Notification.py
@@ -20,18 +20,4 @@ while(True):
             timeout = 5
         )
         percent = cur_per 
-    battery = psutil.sensors_battery()
-    percent = battery.percent
-    time.sleep(60*2)
-    battery = psutil.sensors_battery()
-    cur_per = battery.percent
-    change = cur_per - percent
-    diff = abs(change)
-    #We calculate the change in the battery and show notification if battery level increases or decreases
-    if(diff >= threshold):
-        notification.notify(
-            title = "Battery Percentage",
-            message = str(percent) + "% Battery Remaining",
-            timeout = 5
-        )   
     continue
