5a4a628069f52d39f4e1b4c60e96c01166d795b8
Shubham Gupta <57499043+ShubhamGupta577@users.noreply.github.com>
2021-04-13 00:33:31 +0530

    Create Fast_Algorithm.py

---
+++ Fast Algorithm/Fast_Algorithm.py
@@ -0,0 +1,38 @@
+'''
+  For using this Script you need to install OpenCV in your machine
+'''
+#Importing openCV library
+import cv2 as cv
+
+#Taking path of input from the user
+path=input("Enter the path of uploaded image: ")
+img=cv.imread(path)
+img=cv.resize(img,(640,640))  #resizing the image
+
+#Printing the original image
+cv.imshow('Original',img)
+
+# Initiate FAST object with default values
+fast = cv.FastFeatureDetector_create()
+
+# find and draw the keypoints
+kp = fast.detect(img,None)
+img2 = cv.drawKeypoints(img, kp, None, color=(255,0,0))
+
+# Print all default parameters
+print( "Threshold: {}".format(fast.getThreshold()) )
+print( "nonmaxSuppression:{}".format(fast.getNonmaxSuppression()) )
+print( "neighborhood: {}".format(fast.getType()) )
+print( "Total Keypoints with nonmaxSuppression: {}".format(len(kp)) )
+
+# Disable nonmaxSuppression
+fast.setNonmaxSuppression(0)
+kp = fast.detect(img,None)
+
+print( "Total Keypoints without nonmaxSuppression: {}".format(len(kp)) )
+final = cv.drawKeypoints(img, kp, None, color=(255,0,0))
+
+#Printing the final output image
+cv.imshow('Final',final)
+cv.waitKey(0)
+cv.destroyAllWindows()
