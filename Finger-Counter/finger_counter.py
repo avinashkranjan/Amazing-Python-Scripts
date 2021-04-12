ea306c1134a7cbdf430d1e39cff716104dd0f588
Meha Bhalodiya <73488906+mehabhalodiya@users.noreply.github.com>
2021-04-12 11:42:54 +0530

    Minor change

--- Finger-Counter/finger_counter.py
+++ Finger-Counter/finger_counter.py
@@ -9,7 +9,7 @@ cap = cv2.VideoCapture(1)
 cap.set(3, wCam)
 cap.set(4, hCam)
 
-folderPath = "FingerImages"
+folderPath = "./Finger-Counter/FingerImages"
 myList = os.listdir(folderPath)
 print(myList)
 overlayList = []
