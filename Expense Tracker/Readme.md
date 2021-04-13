a0be21ab54ef58d8c9fc3196900c6733c4c9752b
Amit kumar mishra <akm43300@gmail.com>
2021-04-13 11:48:54 +0530

    Expense tracker

---
+++ Expense Tracker/Readme.md
@@ -0,0 +1,35 @@
+# <b>Expense Tracker</b>
+
+[![forthebadge](https://forthebadge.com/images/badges/made-with-python.svg)](https://forthebadge.com)
+
+## Expense Tracker Functionalities : 🚀
+
+- Enter the directory for the images.
+- Now run the script to see all the images one by one.
+
+## Expense Tracker Instructions: 👨🏻‍💻
+
+### Step 1:
+
+    Open Termnial 💻
+
+### Step 2:
+
+    Locate to the directory where python file is located 📂
+
+### Step 3:
+
+    Run the command: python script.py/python3 script.py 🧐
+
+### Step 4:
+
+    Sit back and Relax. Let the Script do the Job. ☕
+
+## Requirements
+
+    - tkinter
+    - tkcalendar
+
+## Author
+   
+   Amit Kumar Mishra
---
+++ Expense Tracker/script.py
@@ -0,0 +1,61 @@
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import *
+from tkinter.ttk import Notebook
+from tkcalendar import DateEntry
+
+
+def Addexpense():
+    x = Edate.get()
+    y = Title.get()
+    z = Eexpense.get()
+    data = [x,y,z]
+    TVExpense.insert('','end',values=data)
+
+gui = Tk()
+gui.title('Expense Tracker')
+gui.geometry('700x600')
+
+
+Tab = Notebook(gui)
+F1 = Frame(Tab, width=500, height=500)
+
+Tab.add(F1, text="Expense")
+
+
+Tab.pack(fill=BOTH, expand=1)
+
+ldate = ttk.Label(F1, text="Date", font=(None,18))
+ldate.grid(row=0, column=0, padx=5, pady=5, sticky='w')
+
+Edate = DateEntry(F1, width=19, background = 'blue', foreground='white', font=(None,18))
+Edate.grid(row=0, column=1, padx=5,pady=5, sticky='w')
+
+ltitle = ttk.Label(F1, text="Title",font=(None,18))
+ltitle.grid(row=1, column=0, padx=5, pady=5, sticky='w')
+
+Title = StringVar()
+
+Etitle = ttk.Entry(F1, textvariable=Title,font=(None,18))
+Etitle.grid(row=1, column=1, padx=5, pady=5, sticky='w')
+
+lexpense = ttk.Label(F1, text="Expense",font=(None,18))
+lexpense.grid(row=2, column=0, padx=5, pady=5, sticky='w')
+
+Expense = StringVar()
+
+Eexpense = ttk.Entry(F1, textvariable=Expense,font=(None,18))
+Eexpense.grid(row=2, column=1, padx=5, pady=5, sticky='w')
+
+btn = ttk.Button(F1,text='Add', command=Addexpense)
+btn.grid(row=3, column=1, padx=5, pady=5, sticky='w', ipadx=10, ipady=10)
+
+TVList = ['Date','Item','Expense']
+TVExpense = ttk.Treeview(F1, column=TVList, show='headings', height=5)
+
+for i in TVList:
+    TVExpense.heading(i, text=i.title())
+
+TVExpense.grid(row=4, column=0, padx=5, pady=5, sticky='w', columnspan=3)
+
+gui.mainloop()
