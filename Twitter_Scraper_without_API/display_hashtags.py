30c1a00f6b4c453a1c76a08664852fa30430119a
Rohini Rao <64858610+RohiniRG@users.noreply.github.com>
2021-04-15 11:05:44 +0530

    extended filepath

--- Twitter_Scraper_without_API/display_hashtags.py
+++ Twitter_Scraper_without_API/display_hashtags.py
@@ -7,7 +7,7 @@ def sql_connection():
     Establishes a connection to the SQL file database
     :return connection object:
     """
-    path = os.path.abspath('TwitterDatabase.db')
+    path = os.path.abspath('./Twitter_Scraper_without_API/TwitterDatabase.db')
     con = sqlite3.connect(path)
     return con
 
