c70caacb253a6da0873035618f50026ca56c6326
Akhil Bhalerao <55273506+iamakkkhil@users.noreply.github.com>
2021-04-16 23:47:27 +0530

    #913 Codeforces_Problem_Scraper Added

---
+++ Coderforces_Problem_Scrapper/Codeforces_problem_scrapper.py
@@ -0,0 +1,53 @@
+import os
+from selenium import webdriver  # Automated webdriver
+from PIL import Image
+from fpdf import FPDF  # For converting images to pdf
+
+
+def getproblem():
+    """
+    getproblem() : It takes input from the user of codeforces problemID and difficulty
+    level and then by using selenium and chrome webdriver, capturing screenshot of the
+    Codeforces problem using ttypography tag because all the problems of codeforces are
+    stored inside this div tag and saving it in a image.png file.
+    Then saving the image.png as pdf file by using fdf library.
+    """
+
+    # Taking input from the user to search for the problem
+    Pblm_id = input("Enter the Problem ID: ")
+    difficulty = input("Enter the difficulty level: ")
+    filename = input('Enter the file name to store Question: ') + '.pdf'
+
+    # Going to the specific URL
+    url = "https://codeforces.com/problemset/problem/" + Pblm_id + "/" + difficulty
+    path = 'image.png'
+    options = webdriver.ChromeOptions()
+
+    # Headless = True for taking a scrolling snapshot
+    options.headless = True
+    driver = webdriver.Chrome(r"chromedriver_win32\chromedriver.exe", options=options)
+    driver.get(url)
+    # Deciding height by tag
+    required_height = driver.execute_script(
+        'return document.body.parentNode.scrollHeight')
+    driver.set_window_size(1366, required_height)
+
+    # Taking SS of everything within the ttypography class
+    driver.find_element_by_class_name('ttypography').screenshot(path)
+
+    # Opening image with pillow so based to capture its height and width
+    cover = Image.open(path)
+    WIDTH, HEIGHT = cover.size
+    MARGIN = 10
+    # based on image's height and width we are adjusting the pdf margin and borders
+    pdf = FPDF(unit='pt', format=[WIDTH + 2 * MARGIN, HEIGHT + 2 * MARGIN])
+    pdf.add_page()  # Adding new page to the pdf
+    pdf.image(path, MARGIN, MARGIN)
+    pdf.output(filename, "F")  # saving the pdf with the specified filename 
+
+    print(f'\nGreat Success!!! Check your directory for {filename} file!')
+
+
+if __name__ == "__main__":
+    getproblem()
+    os.remove('image.png')
---
+++ Coderforces_Problem_Scrapper/README.md
@@ -0,0 +1,36 @@
+# Save any Problem Statement you like from Codeforces as a PDF.
+
+This python script will let you download Problem Statements from Codeforces and save them as a pdf file. The script uses Selenium Webdriver and fpdf library. Selenium is used with Chrome Webdriver, so having Chrome browser is a requirement.
+
+## Setting up:
+
+- Create a virtual environment and activate it.
+
+- Install the requirements
+
+```sh
+  $ pip install -r requirements.txt
+```
+
+## Running the script:
+
+```sh
+  $ python Codeforces_problem_scrapper.py
+```
+
+## Terminal Screenshot:
+
+![Imgur](https://i.imgur.com/Qr0AwMG.png)
+
+The program will ask you to enter:
+1.  Valid Problem ID.
+2.  Valid Difficulty Level.
+3. filename(without '.pdf'). The pdf will be created in the same folder.
+
+## PDF Output:
+![Imgur](https://i.imgur.com/GpSxCRZ.png)
+![Imgur](https://i.imgur.com/c5mCNWM.png)
+
+## Author
+[Akhil Bhalerao](https://github.com/iamakkkhil)
+
