d39d3a3129aaa6678a2c8d1577f8fe6d8151c56c
nidhivanjare <rnidhivanjare@gmail.com>
2021-04-17 16:49:45 +0530

    Go to followers

--- Instagram Follow- NotFollow/main.py
+++ Instagram Follow- NotFollow/main.py
@@ -32,5 +32,33 @@ class InstaBot:
                 "//button[contains(text(), 'Not Now')]").click()
             sleep(1)
 
+    def get_unfollowers(self):
+        self.driver.find_element_by_xpath(
+            "//a[contains(@href,'/nidhi_vanjare')]").click()
+        sleep(2)
+        self.driver.find_element_by_xpath(
+            "//a[contains(@href,'/following')]").click()
+        sleep(2)
+        # sug = self.driver.find_element_by_xpath(
+        #     '//h4[contains(text(), Suggestions)]')
+        # self.driver.execute_script('arguments[0].scrollIntoView()', sug)
+        # sleep(2)
 
-InstaBot('nidhi_vanjare', 'Instanidhi1307')
+        scroll_box = self.driver.find_element_by_xpath(
+            "/html/body/div[5]/div/div/div[2]")
+        last_ht, ht = 0, 1
+        while last_ht != ht:
+            last_ht = ht
+            sleep(1)
+            ht = self.driver.execute_script(""""
+                arguments[0].scrollTo(0,arguments[0].scrollHeigth);
+                return arguments[0],scrollHeight;
+                """, scroll_box)
+
+
+usr_name = input("Enter Username : ")
+password = input("Enter Password : ")
+
+
+my_bot = InstaBot(usr_name, password)
+my_bot.get_unfollowers()
