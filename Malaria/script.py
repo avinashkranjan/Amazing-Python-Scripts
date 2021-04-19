ddf25055a9a7c3c5b27cb3a6be978189fb7aefe2
Ayush Raj <52033949+ayush-raj8@users.noreply.github.com>
2021-04-19 10:40:02 +0530

    Add files via upload

---
+++ Malaria/script.py
@@ -0,0 +1,126 @@
+
+# importing the libraries for loading data and visualisation
+import os
+import cv2
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline 
+from PIL import Image
+import seaborn as sns
+
+# import for train-test-split
+from sklearn.model_selection import train_test_split
+
+# import for One Hot Encoding
+from keras.utils import to_categorical
+
+# importing libraries for Model
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Dense, Flatten, Dropout, BatchNormalization
+
+# importing libraries for evaluating the model
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+
+# loading the data of images and setting their labels
+data = []
+labels = []
+
+Parasitized = os.listdir("../input/cell-images-for-detecting-malaria/cell_images/Parasitized/")
+
+for a in Parasitized:
+    
+    try:
+        image = cv2.imread("../input/cell-images-for-detecting-malaria/cell_images/Parasitized/" + a)
+        image_from_array = Image.fromarray(image, 'RGB')
+        size_image = image_from_array.resize((50, 50))
+        data.append(np.array(size_image))
+        labels.append(0)
+    
+    except AttributeError:
+        print("")
+
+Uninfected = os.listdir("../input/cell-images-for-detecting-malaria/cell_images/Uninfected/")
+
+for b in Uninfected:
+
+    try:
+        image = cv2.imread("../input/cell-images-for-detecting-malaria/cell_images/Uninfected/" + b)
+        image_from_array = Image.fromarray(image, 'RGB')
+        size_image = image_from_array.resize((50, 50))
+        data.append(np.array(size_image))
+        labels.append(1)
+    
+    except AttributeError:
+        print("")
+
+# Creating single numpy array of all the images and labels
+data = np.array(data)
+labels = np.array(labels)
+
+print('Cells : {} and labels : {}'.format(data.shape , labels.shape))
+
+# lets shuffle the data and labels before splitting them into training and testing sets
+n = np.arange(data.shape[0])
+np.random.shuffle(n)
+data = data[n]
+labels = labels[n]
+
+
+### Splitting the dataset into the Training set and Test set
+
+X_train, X_valid, y_train, y_valid = train_test_split(data, labels, test_size = 0.2, random_state = 0)
+
+X_train = X_train.astype('float32')  
+X_valid = X_valid.astype('float32')
+
+# One Hot Encoding 
+y_train = to_categorical(y_train)
+y_valid = to_categorical(y_valid)
+
+
+classifier = Sequential()
+
+# CNN layers
+classifier.add(Conv2D(32, kernel_size=(3, 3), input_shape = (50, 50, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(BatchNormalization(axis = -1))
+classifier.add(Dropout(0.5))   # Dropout prevents overfitting
+
+classifier.add(Conv2D(32, kernel_size=(3, 3), input_shape = (50, 50, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(BatchNormalization(axis = -1))
+classifier.add(Dropout(0.5))
+
+classifier.add(Flatten())
+
+classifier.add(Dense(units=128, activation='relu'))
+classifier.add(BatchNormalization(axis = -1))
+classifier.add(Dropout(0.5))
+
+classifier.add(Dense(units=2, activation='softmax'))
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+history = classifier.fit(X_train, y_train, batch_size=120, epochs=15, verbose=1, validation_data=(X_valid, y_valid))
+classifier.summary()
+
+y_pred = classifier.predict(X_valid)
+# Convert back to categorical values 
+y_pred = np.argmax(y_pred, axis=1)
+y_valid = np.argmax(y_valid, axis=1)
+classifier.save("./Malaria/Models/malaria.h5")
+
+
+
+
+
+
+
+
+
+
+
