be570b96712043e6dde195411e3db0a7b5c43a3b
LAPTOP-SNSEHSAR\ayush <ayushjain2205@gmail.com>
2021-04-20 22:25:20 +0530

    Scraping and GUI integration complete

--- NSE Stocks GUI/stocks.py
+++ NSE Stocks GUI/stocks.py
@@ -3,7 +3,79 @@ from bs4 import BeautifulSoup
 import tkinter as tk
 from tkinter import ttk
 from tkinter import font as tkFont
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
+driver_path = 'C:\Webdrivers\chromedriver'
+
+# Categories and their URL slugs
+most_active = {'Most Active equities - Main Board':'mae_mainboard_tableC','Most Active equities - SME':'mae_sme_tableC','Most Active equities - ETFs':'mae_etf_tableC',
+                'Most Active equities - Price Spurts':'mae_pricespurts_tableC', 'Most Active equities - Volume Spurts':'mae_volumespurts_tableC'}
+top_20 = {'NIFTY 50 Top 20 Gainers':'topgainer-Table','NIFTY 50 Top 20 Losers':'toplosers-Table'}
+
+# Function to generate request url based on user choice
+def generate_url():
+    category_choice = category.get()
+    if(category_choice in most_active):
+        page = 'most-active-equities'
+    else:
+        page = 'top-gainers-loosers'
+    url = 'https://www.nseindia.com/market-data/{}'.format(page)
+    return url
+
+# Function to scrape stock data from generated URL
+def scraper():
+    url = generate_url()
+    driver = webdriver.Chrome(driver_path)
+    driver.get(url)
+
+    # Wait for results to load
+    time.sleep(5)
+    html = driver.page_source
+
+    # Start scraping resultant html data
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Based on choice scrape div 
+    category_choice = category.get()
+    if category_choice in most_active :
+        category_div = most_active[category_choice]
+    else :
+        category_div = top_20[category_choice]
+
+    # Find the table to scrape 
+    results = soup.find("table", {"id": category_div})
+    rows = results.findChildren('tr')
+
+    table_data = []
+    row_values = []
+    # Append stock data into a list
+    for row in rows:
+        cells = row.findChildren(['th', 'td'])
+        for cell in cells:
+            value = cell.text.strip()
+            value = " ".join(value.split())
+            row_values.append(value)
+        table_data.append(row_values)
+        row_values = []
+
+    # Formatting the stock data stored in the list
+    stocks_data = ""
+    for stock in table_data:
+        single_record = ""
+        for cell in stock:
+            format_cell = "{:<20}"
+            single_record += format_cell.format(cell[:20])
+        single_record += "\n"
+        stocks_data += single_record
+    
+     # Adding the formatted data into tkinter GUI
+    query_label.config(state=tk.NORMAL)
+    query_label.delete(1.0,"end")
+    query_label.insert(1.0,stocks_data)
+    query_label.config(state=tk.DISABLED)
+    driver.close()
 
 # Creating tkinter window
 window = tk.Tk()
@@ -29,7 +101,7 @@ ttk.Label(window, text="Select Market data to get:", background = 'white',
 category = ttk.Combobox(
     window, width=60, state='readonly',font="Helvetica 15")
 
-submit_btn = ttk.Button(window, text="Get Stock Data!", style='my.TButton')
+submit_btn = ttk.Button(window, text="Get Stock Data!", style='my.TButton', command = scraper)
 
 # Adding combobox drop down list
 category['values'] = ('Most Active equities - Main Board','Most Active equities - SME','Most Active equities - ETFs','Most Active equities - Price Spurts',
@@ -43,6 +115,7 @@ submit_btn.grid(row=5, column=3, pady=5, padx=15, ipadx=5)
 frame = ttk.Frame(window, style='my.TFrame')
 frame.place(relx=0.50, rely=0.12, relwidth=0.98, relheight=0.90, anchor="n")
 
+# To display stock data
 query_label = tk.Text(frame ,height="52" ,width="500", bg="alice blue")
 query_label.grid(row=7,  columnspan=2)
 
