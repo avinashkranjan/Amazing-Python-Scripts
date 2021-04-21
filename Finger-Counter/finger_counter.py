3a6754d1917e15181bc31640dfba82a3fbb24cac
Meha Bhalodiya <mehabhalodiya@gmail.com>
2021-04-21 15:13:11 +0530

    Modified finger_counter.py

--- Finger-Counter/finger_counter.py
+++ Finger-Counter/finger_counter.py
@@ -3,68 +3,69 @@ import time
 import os
 import HandTrackingModule as htm
 
-wCam, hCam = 640, 480
+class fingerCounter():
+    def __init__(self):
+        self.wCam = 640
+        self.hCam = 480
+        self.folderPath = "FingerImages"
+        self.previousTime = 0
+        self.currentTime = time.time()
+        # 4 for thumb, 8 for index, 12 for middle, 16 for ring, 20 for pinky finger
+        self.tipIds = [4, 8, 12, 16, 20]
 
-cap = cv2.VideoCapture(1)
-cap.set(3, wCam)
-cap.set(4, hCam)
+    def image_read(self, overlayList):
+        myList = os.listdir(self.folderPath)
+        print(myList)
 
-folderPath = "./Finger-Counter/FingerImages"
-myList = os.listdir(folderPath)
-print(myList)
-overlayList = []
+        for imPath in myList:
+            image = cv2.imread(f'{self.folderPath}/{imPath}')
+            overlayList.append(image)
 
-for imPath in myList:
-    image = cv2.imread(f'{folderPath}/{imPath}')
-    overlayList.append(image)
+        print(len(overlayList))
 
-print(len(overlayList))
+    def process(self, tipIds, overlayList):
+        cap = cv2.VideoCapture(1)
+        cap.set(3, self.wCam)
+        cap.set(4, self.hCam)
+        detector = htm.handDetector(detectionCon=0.75)
+        while True:
+            success, img = cap.read()
+            img = detector.findHands(img)
+            lmList = detector.findPosition(img, draw = False)
+            print(lmList)
 
-previousTime = 0
+            if len(lmList) != 0:
+                fingers = []
 
-detector = htm.handDetector(detectionCon = 0.75)
+                # Thumb
+                if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][2]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
 
-# 4 for thumb, 8 for index, 12 for middle, 16 for ring, 20 for pinky finger
-tipIds = [4, 8, 12, 16, 20]
+                # Four fingers
+                for id in range(1,5):
+                    if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                        fingers.append(1)
+                    else:
+                        fingers.append(0)
 
-while True:
-    success, img = cap.read()
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw = False)
-    print(lmList)
+                totalFingers = fingers.count(1)
+                print(totalFingers)
 
-    if len(lmList) != 0:
-        fingers = []
+                h, w, c = overlayList[totalFingers - 1].shape
+                img[0:h, 0:w] = overlayList[totalFingers - 1]
 
-        # Thumb
-        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][2]:
-            fingers.append(1)
-        else:
-            fingers.append(0)
+                cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 25)
 
-        # Four fingers
-        for id in range(1,5):
-            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
+            fps = 1/(self.currentTime - self.previousTime)
+            self.previousTime = self.currentTime
 
-        totalFingers = fingers.count(1)
-        print(totalFingers)
+            cv2.putText(img, f'FPS: {int(fps)}', (400,70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
-        h, w, c = overlayList[totalFingers - 1].shape
-        img[0:h, 0:w] = overlayList[totalFingers - 1]
+            cv2.imshow("Image", img)
+            cv2.waitKey(1)
 
-        cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
-        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 25)
-
-    currentTime = time.time()
-    fps = 1/(currentTime - previousTime)
-    previousTime = currentTime
-
-    cv2.putText(img, f'FPS: {int(fps)}', (400,70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
-
-
+if __name__ == "__main__":
+    counter = fingerCounter()
