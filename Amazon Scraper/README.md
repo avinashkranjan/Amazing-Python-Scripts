a1a2d5475614d99b6c9eb5e6b46c014f353660b7
ARNAB RAY <59610398+Arnab11917676@users.noreply.github.com>
2021-04-21 10:56:59 +0530

    Done

--- Amazon Scraper/README.md
+++ Amazon Scraper/README.md
@@ -1,10 +1,11 @@
-### Amazon Scraper
-This is a python-amazon scraper is expected to work on any amazon.in link provided to it and will update the information accordingly. 
+# Amazon Item Scraper
+
+This is a python-amazon scraper is expected to work on any amazon.in link provided to it and will update the information accordingly.
 In this scraper I have used the Requests library for the generating the requersts and the scientific libraries of BeautifuSoup and Pandas for generating the Data Frames.
 I have also used the time library so that the programm and auto update the stocks according to the user's wish.
- 
+
 Demo Link: https://www.youtube.com/watch?v=DTR9-uBqxtY
 
 # Output
-![image](https://user-images.githubusercontent.com/59610398/115266387-cbe4e580-a155-11eb-8ba5-4b2f7f15f104.png)
 
+![image](https://user-images.githubusercontent.com/59610398/115266387-cbe4e580-a155-11eb-8ba5-4b2f7f15f104.png)
--- Amazon Scraper/scrape.py
+++ Amazon Scraper/scrape.py
@@ -1,26 +1,31 @@
+#libraries used
+
+
 import csv
-
 import pandas as pd
-
 from bs4 import BeautifulSoup
-
 import requests
-
 import time
 
+
+#main Scraper Function
 def scraper():
 
+
+    #the csv file deceared
     f=open('amazon.csv','w',newline='')
     data=csv.writer(f)
+    main = []
     headers = (
         {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
         'Accept-language': 'en-US,en;q=0.5'
         }      )
-    main=[]
     preq=requests.get(url_name,headers)
-
     soup=BeautifulSoup(preq.content,features="html.parser")
+
+
+    #scraping the details
     product_author_name=[i.text for i in soup.findAll("span",{"class":"a-size-small a-color-base"})]
     data.writerow(product_author_name)
     main.append(product_author_name)
@@ -28,22 +33,25 @@ def scraper():
     data.writerow(product_rating)
     main.append(product_rating)
 
+
+    #creating the DataFrame
     df=pd.DataFrame(data=main)
     df.to_csv('amazon_report.csv',index=False,header=False)
 
+
+    #completion message
     print('Done')
     time.sleep(time_interval)
 
 if __name__ == '__main__':
 
+    #inputs for the programm
     take_max_run_time=int(input("Enter the time in 'SECOND' you want the scraper to work for the upadtion purpose:- "))
-
     time_interval=int(input("Enter the time you want to hold the scraper : "))
-
     url_name = input('Enter the link: ')
 
+
+    #module thats makes it run for the desired time
     end_time=time.time() + take_max_run_time
-
     while time.time() < end_time:
-
         scraper()
