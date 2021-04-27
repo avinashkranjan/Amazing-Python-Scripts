f330b27fcb3bd9bc56e0460556b6c62e080d2936
sayantani11 <ii.sayantani.ii@gmail.com>
2021-04-27 19:41:56 +0530

    Changes made

--- Covid-19 _Real-time_Notification/Covid.py
+++ Covid-19 _Real-time_Notification/Covid.py
@@ -16,23 +16,24 @@ def getInfo(url):
 
 
 if __name__ == '__main__':
+    states = list(map(str,input("Enter the name of states:")))
     while True:
         HtmlData = getInfo('https://www.mohfw.gov.in/')
         soup = BeautifulSoup(HtmlData, 'html.parser')
     
         myData=""
-        for tr in soup.find('table').find('tbody')[1].find_all('tr'):
+        for tr in soup.find_all('tbody')[1].find_all('tr'):
             myData += tr.get_text()
         
         myData = myData[1:]
         itemList = myData.split("\n\n")
     
-        states = list(map(str,input("Enter the name of state:"))
         for item in itemList[0:22]:
             dataList = item.split('\n')
             if dataList[1] in states:
                 print(dataList)
                 nTitle = 'Cases of Covid-19'
-                ntext = f"{dataList[1]} Total: {datalist[2][0]}\n Cured: {datalist[4][0]}\n Deaths: {datalist[6][0]}"
+                ntext = f"{dataList[1]} Total: {datalist[2]}\n Cured: {datalist[3]}\n Deaths: {datalist[4]}"
                 notify_user(nTitle, ntext)
+                time.sleep(2)
         time.sleep(3600)
