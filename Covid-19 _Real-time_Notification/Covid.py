e6ad7fdf882072db631cc383e55a6f80c50ff103
sayantani11 <ii.sayantani.ii@gmail.com>
2021-04-27 20:49:00 +0530

    Changes

--- Covid-19 _Real-time_Notification/Covid.py
+++ Covid-19 _Real-time_Notification/Covid.py
@@ -16,24 +16,24 @@ def getInfo(url):
 
 
 if __name__ == '__main__':
-    states = list(map(str,input("Enter the name of states:")))
-    while True:
-        HtmlData = getInfo('https://www.mohfw.gov.in/')
-        soup = BeautifulSoup(HtmlData, 'html.parser')
-    
-        myData=""
-        for tr in soup.find_all('tbody')[1].find_all('tr'):
-            myData += tr.get_text()
+        states = list(map(str,input("Enter the name of states:").split(",")))
         
-        myData = myData[1:]
-        itemList = myData.split("\n\n")
-    
-        for item in itemList[0:22]:
-            dataList = item.split('\n')
-            if dataList[1] in states:
-                print(dataList)
-                nTitle = 'Cases of Covid-19'
-                ntext = f"{dataList[1]} Total: {datalist[2]}\n Cured: {datalist[3]}\n Deaths: {datalist[4]}"
-                notify_user(nTitle, ntext)
-                time.sleep(2)
-        time.sleep(3600)
+        while True:
+            HtmlData = getInfo('https://www.medtalks.in/live-corona-counter-india')
+            soup = BeautifulSoup(HtmlData, 'html.parser')
+        
+            myData = ""
+            for tr in soup.find('tbody').find_all('tr'):
+                myData += tr.get_text()
+            myData = myData[1:]
+            itemList = myData.split("\n\n")
+            for item in itemList[:-2]:
+                dataList = item.split('\n')
+            
+                if dataList[0] in states:
+                    print(dataList)
+                    nTitle = 'Cases of Covid-19'
+                    nText = f"State: {dataList[0]}: Total: {dataList[1]}\n Active: {dataList[2]}\n Death: {dataList[3]}"
+                    notify_user(nTitle, nText)
+                    time.sleep(2)
+            time.sleep(3600)
