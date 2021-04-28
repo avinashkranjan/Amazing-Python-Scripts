b4e279974a555d1d2f5b5242770f6c1331e44fb7
ARNAB RAY <59610398+Arnab11917676@users.noreply.github.com>
2021-04-28 19:12:43 +0530

    Sinchan Drawing Using Python

---
+++ Sinchan Drawing Using Python/run.py
@@ -0,0 +1,22 @@
+import time
+
+import pygame
+
+from sinchan import *
+from pygame import mixer
+mixer.init()
+mixer.music.load('F:\\dataScience\\Bot\\sinchanTurtle\\mainFile\\sinchan_music.mpeg')
+mixer.music.play()
+short()
+legs()
+shirt()
+head()
+hands()
+mouth()
+eyebrows()
+eyes()
+robo()
+ht()
+time.sleep(5)
+exit()
+
