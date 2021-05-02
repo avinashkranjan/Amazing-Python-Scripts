188acc3f7fc6ca929b68c58e627c567d77b62f20
Shubhrima Jana <68013183+Shubhrima@users.noreply.github.com>
2021-05-02 12:17:44 +0530

    Update main.py

--- Rain Alert Mail Sender/main.py
+++ Rain Alert Mail Sender/main.py
@@ -1,20 +1,21 @@
 import requests
 from smtplib import SMTP
+import time
 
 #turn off certain security criteria in sender mail address
 MY_MAIL= input('Enter your mail id: ')
 MY_PASSWORD= input('Enter password: ')
 RECIEVER_MAIL = input('Send mail to (mail id): ')
+#get latitude longitude from  "https://www.latlong.net/"
 CITY = input('Enter your City: ')
 
-API_KEY = input('Type in API from OpenWeather: ')
+API_KEY = input('Type in API key from OpenWeather: ')
 
 API_END_POINT ='https://nominatim.openstreetmap.org/search.php'
 PARAMETER_LOCATION ={
     'city' : CITY,
     'format': 'json',
 }
-
 response_location  = requests.get(url = API_END_POINT, params=PARAMETER_LOCATION)
 data_location = response_location .json()
 LAT = data_location [0]['lat']
@@ -37,16 +38,14 @@ for i in range(0,12):
         bring_umbrella = True
 
 if (bring_umbrella == True):
-    with SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=MY_MAIL, password=MY_PASSWORD)
-        connection.sendmail(from_addr=MY_MAIL, to_addrs=RECIEVER_MAIL,
-                            msg=f"Subject: Rain Rain \n\nIt's going to rain today. Bring Umbrella ")
-        print('Mail Sent')
+    MESSAGE=f"Subject: Rain Rain \n\nIt's going to rain today. Bring Umbrella "
+
 else:
-    with SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=MY_MAIL, password=MY_PASSWORD)
-        connection.sendmail(from_addr=MY_MAIL, to_addrs='shubhrijana@gmail.com',
-                            msg=f"Subject: Sunny Day\n\nMay be a sunny day. Carry sunglasses. ")
-        print('Mail Sent')
+    MESSAGE = f"Subject: Sunny Day\n\nMay be a sunny day. Carry sunglasses. "
+
+with SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=MY_MAIL, password=MY_PASSWORD)
+    connection.sendmail(from_addr=MY_MAIL, to_addrs=RECIEVER_MAIL,
+                        msg=MESSAGE)
+    print('Mail Sent')
