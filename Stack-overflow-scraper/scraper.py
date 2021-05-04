0812e04611fffce11d1bf4ffdde4240bcdfe0bec
LAPTOP-SNSEHSAR\ayush <ayushjain2205@gmail.com>
2021-05-05 03:16:06 +0530

    DB integrated with UI

--- Stack-overflow-scraper/scraper.py
+++ Stack-overflow-scraper/scraper.py
@@ -5,6 +5,34 @@ from tkinter import messagebox, simpledialog
 from tkinter import ttk
 from tkinter import font as tkFont
 import time
+import sqlite3
+from sqlite3 import Error
+
+# Function to connect to the SQL Database
+def sql_connection():
+    try:
+        con = sqlite3.connect('./Stack-overflow-scraper/stackoverflow.db')
+        return con
+    except Error:
+        print(Error)
+
+# Function to create table
+def sql_table(con):
+    cursorObj = con.cursor()
+    cursorObj.execute(
+        "CREATE TABLE IF NOT EXISTS questions(question_text text, question_summary text, question_link text,votes integer, views integer )")
+    con.commit()
+
+# Call functions to connect to database and create table
+con = sql_connection()
+sql_table(con)
+
+# Function to insert into table
+def sql_insert(con, entities):
+    cursorObj = con.cursor()
+    cursorObj.execute(
+        'INSERT INTO questions(question_text, question_summary, question_link, votes, views) VALUES(?, ?, ?, ?, ?)', entities)
+    con.commit()
 
 # Function to generate URL based on choice
 def get_URL():
@@ -37,13 +65,57 @@ def scrape_questions():
         return ""
     for question in questions:
         question_text = question.find('a', {'class': 'question-hyperlink'}).text.strip()
-        question_link =  'https://stackoverflow.com{}'.format(question.find('a', {'class': 'question-hyperlink'})['href'])
         question_summary = question.find('div', {'class': 'excerpt'}).text.strip()
+        question_summary = question_summary.replace('\n',' ')
+        question_link =  'https://stackoverflow.com{}'.format(question.find('a', {'class': 'question-hyperlink'})['href'])
         votes = question.find('span', {'class': 'vote-count-post'}).text.strip()
         views = question.find('div', {'class': 'views'}).text.strip().split()[0]
-
+        entities = (question_text, question_summary, question_link, votes, views)
+        sql_insert(con, entities)
+        
     messagebox.showinfo("Success!", "Questions scrapped successfully!") 
     clear_progress()
+
+# Function to fetch stackoverflow questions from DB
+def sql_fetch(con):
+    cursorObj = con.cursor()
+    try:
+        cursorObj.execute('SELECT DISTINCT * FROM questions ORDER BY rowid DESC')  # SQL search query
+    except Error:
+        print("Database empty... Fetch users using GUI")
+        return
+
+    rows = cursorObj.fetchall()
+    display_text = ""
+
+    # Show messagebox incase of empty DB
+    if len(rows) == 0 :
+        messagebox.showinfo("Alert", "No users scraped yet!")
+        return " "
+
+    first_row = "{:^65}".format("Question") + "{:^65}".format("Summary") + "{:^40}".format("Link") + "{:^15}".format("Votes") + "{:^15}".format("Views") + '\n'
+    display_text += first_row
+
+    # Format rows
+    for row in rows:
+        question_text = "{:<65}".format(
+            row[0] if len(row[0]) < 60 else row[0][:56]+"...")
+        question_summary = "{:<65}".format(
+            row[1] if len(row[1]) < 60 else row[1][:56]+"...")
+        question_link = "{:<40}".format(
+            row[2] if len(row[2]) < 30 else row[2][:36]+"...")
+        votes = "{:^15}".format(row[3])
+        views = "{:^15}".format(row[4])
+        display_text += (question_text + question_summary + question_link + votes + views +'\n')
+
+    return display_text
+
+def show_results():
+    display_text = sql_fetch(con)
+    query_label.config(state=tk.NORMAL)
+    query_label.delete(1.0, "end")
+    query_label.insert(1.0, display_text)
+    query_label.config(state=tk.DISABLED)
 
 def clear_progress():
     #set progress bar back to 0
@@ -81,7 +153,7 @@ ttk.Label(window, text="Enter tag (ex - python):", background = 'white',
 scrape_btn = ttk.Button(window, text="Scrape questions!", style='my.TButton', command=scrape_questions)
 scrape_btn.grid(row=5, column=2, pady=5, padx=15, ipadx=5)
 
-display_btn = ttk.Button(window, text="Display from DB", style='my.TButton')
+display_btn = ttk.Button(window, text="Display from DB", style='my.TButton', command = show_results)
 display_btn.grid(row=7, column=2, pady=5, padx=15, ipadx=5)
 
 # Search Box
