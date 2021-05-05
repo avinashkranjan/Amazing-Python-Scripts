e2ba190da456574f9c64aca437b02dc24b97a21f
Meha Bhalodiya <mehabhalodiya@gmail.com>
2021-05-05 11:04:33 +0530

    Update finger_counter.py

--- Finger-Counter/finger_counter.py
+++ Finger-Counter/finger_counter.py
@@ -9,9 +9,12 @@ class fingerCounter():
         self.hCam = 480
         self.folderPath = "FingerImages"
         self.previousTime = 0
-        self.currentTime = time.time()
+        #self.currentTime = time.time()
         # 4 for thumb, 8 for index, 12 for middle, 16 for ring, 20 for pinky finger
         self.tipIds = [4, 8, 12, 16, 20]
+        self.overlayList = []
+        self.image_read(self)
+        self.process(self, self)
 
     def image_read(self, overlayList):
         myList = os.listdir(self.folderPath)
@@ -19,9 +22,8 @@ class fingerCounter():
 
         for imPath in myList:
             image = cv2.imread(f'{self.folderPath}/{imPath}')
-            overlayList.append(image)
-
-        print(len(overlayList))
+            self.overlayList.append(image)
+        print(len(self.overlayList))
 
     def process(self, tipIds, overlayList):
         cap = cv2.VideoCapture(1)
@@ -38,14 +40,13 @@ class fingerCounter():
                 fingers = []
 
                 # Thumb
-                if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][2]:
+                if lmList[self.tipIds[0]][1] > lmList[self.tipIds[0] - 1][2]:
                     fingers.append(1)
                 else:
                     fingers.append(0)
-
                 # Four fingers
                 for id in range(1,5):
-                    if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                    if lmList[self.tipIds[id]][2] < lmList[self.tipIds[id] - 2][2]:
                         fingers.append(1)
                     else:
                         fingers.append(0)
@@ -53,12 +54,12 @@ class fingerCounter():
                 totalFingers = fingers.count(1)
                 print(totalFingers)
 
-                h, w, c = overlayList[totalFingers - 1].shape
-                img[0:h, 0:w] = overlayList[totalFingers - 1]
-
+                h, w, c = self.overlayList[totalFingers - 1].shape
+                img[0:h, 0:w] = self.overlayList[totalFingers - 1]
                 cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
                 cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 25)
-
+            
+            self.currentTime = time.time()
             fps = 1/(self.currentTime - self.previousTime)
             self.previousTime = self.currentTime
 
