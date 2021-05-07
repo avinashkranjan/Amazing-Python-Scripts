8c349a8bdedc193cb3da33989f451ad5ada08113
LAPTOP-SNSEHSAR\ayush <ayushjain2205@gmail.com>
2021-05-08 05:01:24 +0530

    Search functionality updated

--- website/scripts.js
+++ website/scripts.js
@@ -1,35 +1,43 @@
-const requestURL = "datastore.json";
-const request = new XMLHttpRequest();
-request.open("GET", requestURL);
-request.responseType = "json";
-request.send();
-request.onload = processData;
+document.addEventListener("DOMContentLoaded", function (event) {
+  const requestURL = "datastore.json";
+  const request = new XMLHttpRequest();
+  request.open("GET", requestURL);
+  request.responseType = "json";
+  request.send();
+  request.onload = processData;
 
-function processData() {
-  const data = request.response;
-  categories = data[0];
-  let scriptNumber = 1;
-  for (category in categories) {
-    var category_scripts = categories[category];
-    for (script in category_scripts) {
-      var scriptDetails = {
-        name: script,
-        summary: category_scripts[script][5],
-        author: category_scripts[script][4],
-        folder: category_scripts[script][0],
-        file: category_scripts[script][1],
-      };
-      populateCards(scriptNumber, scriptDetails);
-      scriptNumber++;
+  function processData() {
+    const data = request.response;
+    categories = data[0];
+    let scriptNumber = 1;
+    for (category in categories) {
+      var category_scripts = categories[category];
+      for (script in category_scripts) {
+        var scriptDetails = {
+          name: script,
+          summary: category_scripts[script][5],
+          author: category_scripts[script][4],
+          folder: category_scripts[script][0],
+          file: category_scripts[script][1],
+        };
+        populateCards(scriptNumber, scriptDetails);
+        scriptNumber++;
+      }
     }
   }
-}
 
-function populateCards(scriptNumber, scriptDetails) {
-  let cardHolder = document.getElementById("card-holder");
-  let isHidden = scriptNumber <= 20 ? "" : "hidden";
+  // Bind event handlers to buttons
+  let loadMore = document.getElementById("load-more");
+  loadMore.addEventListener("click", More);
 
-  let divHTML = `<div class="col-sm-6 col-md-3 ${isHidden} ">
+  let search = document.getElementById("search");
+  search.addEventListener("keypress", Search);
+
+  function populateCards(scriptNumber, scriptDetails) {
+    let cardHolder = document.getElementById("card-holder");
+    let isHidden = scriptNumber <= 20 ? "" : "hidden";
+
+    let divHTML = `<div class="col-sm-6 col-md-3 ${isHidden} ">
                       <div class="card shadow">
                           <div class="card-body">
                               <h5 class="card-title">${scriptDetails["name"]}</h5>
@@ -39,16 +47,31 @@ function populateCards(scriptNumber, scriptDetails) {
                           </div>
                       </div>
                   </div>`;
-  cardHolder.innerHTML += divHTML;
-}
-
-var button = document.getElementById("Load");
-
-function More() {
-  button.style.display = "none";
-  let cardsCount = document.getElementById("card-holder").children.length;
-  let cards = document.getElementById("card-holder").children;
-  for (let count = 0; count < cardsCount; count++) {
-    cards[count].style.display = "flex";
+    cardHolder.innerHTML += divHTML;
   }
-}
+
+  function More() {
+    loadMore.style.display = "none";
+    let cardsCount = document.getElementById("card-holder").children.length;
+    let cards = document.getElementById("card-holder").children;
+    for (let count = 0; count < cardsCount; count++) {
+      cards[count].style.display = "flex";
+    }
+  }
+
+  function Search() {
+    let cardsCount = document.getElementById("card-holder").children.length;
+    let cards = document.getElementById("card-holder").children;
+    let searchKey = document.getElementById("search").value.toLowerCase();
+    for (let count = 0; count < cardsCount; count++) {
+      scriptTitle = cards[count]
+        .getElementsByTagName("h5")[0]
+        .innerHTML.toLowerCase();
+      if (scriptTitle.includes(searchKey)) {
+        cards[count].style.display = "flex";
+      } else {
+        cards[count].style.display = "none";
+      }
+    }
+  }
+});
