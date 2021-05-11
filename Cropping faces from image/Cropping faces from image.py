2763502fea2173c8104f5b606f869b8516f47a57
Garima Goyal <57147709+undetectablevirus@users.noreply.github.com>
2021-05-11 17:26:41 +0530

    Update Cropping faces from image.py

--- Cropping faces from image/Cropping faces from image.py
+++ Cropping faces from image/Cropping faces from image.py
@@ -1,13 +1,16 @@
 import cv2                           #importing cv library
-import numpy as np                   #importing numpy library as np                      
+import numpy as np                   #importing numpy library as np   
+import os                            #importing os module
 
 path = input('Enter the path for image: ')   #Taking image path from user
 
 img = cv2.imread(path)
   
-# Convert into grayscale
+# Convert image into grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)   
 
+#cascade loading
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 # Detect faces
@@ -21,6 +24,11 @@ for (x, y, w, h) in faces:
     cv2.imshow('Cropped faces',faces)     #Displaying the cropped faces
    
   
-cv2_imshow('Original Image',img)              #Displaying the original Image
+cv2.imshow('Original Image',img)              #Displaying the original Image
+
+#Saving the image of cropped faces
+directory = input('Enter the directory where you want to save the image: ')    #Taking input for desired directory
+os.chdir(directory)
+cv2.imwrite('Cropped faces.jpg',faces)   #saving the image
 cv2.waitKey()
 cv2.destroyAllWindows()
