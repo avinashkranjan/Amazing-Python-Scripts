79cc5cc9b9575e25d5ff362d57a83724513743f7
daretohack <cseanamika3@gmail.com>
2021-05-24 18:29:21 +0530

    Added files for Facebook-DP-Downloader

---
+++ Facebook-DP-Downloader/Readme.md
@@ -0,0 +1,16 @@
+# Facebook Profile Picture Downloader
+
+Download the profile picture of any public profile on Facebook by its Facebook id.
+<br>
+
+## How it works:
+
+* Just run the script by entering the Facebook id (valid Facebook-user-id) and get the profile-pictures stored in your local file.
+* The Facebook id can be thought of as a unique Facebook-user-id of any profile existing on Facebook.
+* This method allows to download profile pictures from only those profile, which are made public along with no profile picture guard set.
+* The profiles begin to exist on Facebook from Facebook user id=4. No profile pictures will get downloaded for Facebook ids below 4, since there exists no such Facebook user id.
+* The profile pictures will only get downloaded if the respective Facebook profile exists for the corresponding Facebook user id entered (the profile does may/may not exist for every numerical value).
+* User-id-4 is for the Facebook id of [Mark Zuckerberg](https://en.wikipedia.org/wiki/Mark_Zuckerberg)
+<br><br>
+
+Script by [Anamika](https://github.com/noviicee)
---
+++ Facebook-DP-Downloader/fb_dp_downloader.py
@@ -0,0 +1,39 @@
+"""
+    Facebook-DP-Downloader
+        Download the profile picture of any public profile on Facebook
+        by just having it's Facebook id 
+    
+"""
+
+import os
+import requests
+
+url="https://graph.facebook.com/{}/picture?type=large"
+
+""" This url is the url provided by the Facebook graph api
+which helps to get to the profile picture of the corresponding Facebook id 
+{}==Facebook id 
+Facebook id  denotes the unique user id of the Facebook profile
+whose profile we are requesting
+"""
+
+path = os.getcwd()
+# get the path of the current working directory
+
+if not "fb_dps" in os.listdir(path):
+    os.mkdir("fb_dps")
+
+"""checks if the folder exists in the current working directory.
+If it does not exist, then it gets created
+"""
+
+fbid=int(input("Enter the Facebook-id to download it's profile picture: "))
+# the number should be a valid Facebook user id 
+
+try:
+    result=requests.get(url.format(fbid))
+    with open("fb_dps/{}_img.jpg".format(fbid),"wb") as file:
+        file.write(result.content)
+
+except:
+	print("There was some error")
