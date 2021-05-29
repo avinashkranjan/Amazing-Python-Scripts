7091d61960d2ff016d160ebcd89e49156098f593
Sukriti-sood <soodsukriti1111@gmail.com>
2021-05-29 13:22:00 +0530

    implemented progress bar

--- Linkedin_Video_Downloader/script.py
+++ Linkedin_Video_Downloader/script.py
@@ -3,11 +3,15 @@
 import tkinter as tk
 import requests as req
 import html
+import time
+from tkinter.ttk import *
+from threading import Thread
+import queue
+from queue import Empty
 
 
 def Invalid_Url():
     """ Sets Status bar label to error message """
-    Download_Window.insert(tk.END,f"Invalid URL")
     Status["text"] = "Invalid URL..."
     Status["fg"] = "red"
 
@@ -15,7 +19,6 @@ def Invalid_Url():
 def Download_vid():
 
     # Validates Link and download Video
-    Download_Window.delete("0.0", "end")
     global Url_Val
     url=Url_Val.get()
 
@@ -44,37 +47,71 @@ def Download_vid():
         if "dms.licdn.com" in source:
 
             videourl = source.split(',')[0].split('"src":')[1][1:-1]
+            start_downloading()
 
-            r = req.get(videourl, stream=True)
-
-            total_size = int(r.headers.get('content-length', 0))
-
-            block_size = 1024  
-
-            with open('video.mp4', 'wb') as file:
-                totaldata=0;
-                for data in r.iter_content(block_size):
-                    totaldata+=len(data)
-                    per_downloaded=totaldata*100/total_size
-                    Download_Window.delete("1.0","end")
-                    Download_Window.insert(tk.END,f"Dowloaded.... {per_downloaded}%")
-                    file.write(data)
-                print("Download Finished")
+            download_thread=VideoDownload(videourl)
+            download_thread.start()
+            monitor(download_thread)
             break  
 
-    Status["text"] = "Finished!!"
-    Status["fg"] = "green"
 
+class VideoDownload(Thread):
+    
+    def __init__(self, url):
+        super().__init__()
+
+        self.url = url
+
+    def run(self):
+        """ download video"""
+
+        # save the picture to a file
+        block_size = 1024  # 1kB
+        r = req.get(self.url, stream=True)
+        total_size = int(r.headers.get("content-length"))
+
+        with open('video.mp4', 'wb') as file:
+            totaldata=0;
+            for data in r.iter_content(block_size):
+                totaldata+=len(data)
+                per_downloaded=totaldata*100/total_size
+                queue.put(per_downloaded)
+                bar['value'] = per_downloaded
+                file.write(data)
+                time.sleep(0.01)
+            file.close()    
+            print("Download Finished")
+
+        print("Download Complete !!!")
+        Status["text"] = "Finished!!"
+        Status["fg"] = "green"
+
+
+#start download
+def start_downloading():
+   bar["value"]=0;
+
+def monitor( download_thread):
+    """ Monitor the download thread """
+    if download_thread.is_alive():
+
+        try:
+            bar["value"]=queue.get(0)
+            ld_window.after(10, lambda: monitor(download_thread))
+        except Empty:
+            pass
 
 # GUI
 
 ld_window=tk.Tk()
 ld_window.title("Linkedin Video Downloader")
-ld_window.geometry("400x400")
+ld_window.geometry("400x300")
 
 # Label for URL Input
 input_label= tk.Label(ld_window,text="Enter Linkedin Video URL:")
 input_label.pack()
+
+queue=queue.Queue()
 
 # Input of URL
 Url_Val = tk.StringVar()
@@ -85,11 +122,10 @@ Url_Input.place( x=25,y=50, width=350)
 Download_button = tk.Button(ld_window, text="Download", font=("Calibri", 9), command=Download_vid)
 Download_button.place(x=100, y=100, width=200)
 
-# Download Window
+# Progress Bar
+bar = Progressbar(ld_window, length=350, style='grey.Horizontal.TProgressbar',mode='determinate')
+bar.place(y=200,width=350,x=25)
 
-Download_Window = tk.Text(ld_window, font=("Calibri", 9), bg="black", fg="white", bd=1, relief=tk.SUNKEN, wrap=tk.WORD)
-Download_Window.insert(tk.END, "Welcome to Linkedin Video Downloader, Provide a Linkedin post link in the above box and click download to start the process. :D")
-Download_Window.place(x=25, y=200, width=350, height=250)
 
 # Text for Status of Downloading
 Status = tk.Label(ld_window, text="Hello!! :D", fg="blue", font=("Calibri", 9), bd=1, relief=tk.SUNKEN, anchor=tk.W, padx=3)
