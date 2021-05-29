e393c9b5ab2e0e7732dbe490446208019524a5f3
Sukriti-sood <soodsukriti1111@gmail.com>
2021-05-29 13:09:13 +0530

    implemented progress bar in GUI

--- Facebook_Video_Downloader/script.py
+++ Facebook_Video_Downloader/script.py
@@ -1,10 +1,13 @@
 # ALL Imports
+import time
 from tkinter.ttk import *
 import tkinter as tk
 from requests import get, HTTPError, ConnectionError
 from re import findall
 from urllib.parse import unquote
-
+from threading import Thread
+import queue
+from queue import Empty
 
 def Invalid_Url():
     """ Sets Status bar label to error message """
@@ -48,36 +51,68 @@ def Download_vid():
 
     link=get_downloadlink(url)
 
-    download(link)
+    start_downloading()
 
-    Status["text"] = "Finished!!"
-    Status["fg"] = "green"
+    download_thread=VideoDownload(link)
+    download_thread.start()
+    monitor(download_thread)
 
-def download(url):
-    block_size = 1024  # 1kB
-    r = get(url, stream=True)
-    total_size = int(r.headers.get("content-length"))
 
-    with open('video.mp4', 'wb') as file:
-        totaldata=0;
-        for data in r.iter_content(block_size):
-            totaldata+=len(data)
-            per_downloaded=totaldata*100/total_size
-            bar['value'] = per_downloaded
-            file.write(data)
-        file.close()    
-        print("Download Finished")
 
-    print("Download Complete !!!")
+def monitor( download_thread):
+    """ Monitor the download thread """
+    if download_thread.is_alive():
 
-    pass
+        try:
+            bar["value"]=queue.get(0)
+            ld_window.after(10, lambda: monitor(download_thread))
+        except Empty:
+            pass
 
+
+
+class VideoDownload(Thread):
+
+    def __init__(self, url):
+        super().__init__()
+
+        self.url = url
+
+    def run(self):
+        """ download video"""
+
+        # save the picture to a file
+        block_size = 1024  # 1kB
+        r = get(self.url, stream=True)
+        total_size = int(r.headers.get("content-length"))
+
+        with open('video.mp4', 'wb') as file:
+            totaldata=0;
+            for data in r.iter_content(block_size):
+                totaldata+=len(data)
+                per_downloaded=totaldata*100/total_size
+                queue.put(per_downloaded)
+                bar['value'] = per_downloaded
+                file.write(data)
+                time.sleep(0.01)
+            file.close()    
+            print("Download Finished")
+
+        print("Download Complete !!!")
+        Status["text"] = "Finished!!"
+        Status["fg"] = "green"
+
+
+
+#start download
+def start_downloading():
+   bar["value"]=0;
 
 # GUI
 
 ld_window=tk.Tk()
 ld_window.title("Facebook Video Downloader")
-ld_window.geometry("400x400")
+ld_window.geometry("400x300")
 
 # Label for URL Input
 input_label= tk.Label(ld_window,text="Enter Facebook Video URL:")
@@ -94,12 +129,11 @@ Download_button.place(x=100, y=100, width=200)
 
 # Progress Bar
 bar = Progressbar(ld_window, length=350, style='grey.Horizontal.TProgressbar',mode='determinate')
-bar['value'] = 0
 bar.place(y=200,width=350,x=25)
 
-
+queue=queue.Queue()
 # Text for Status of Downloading
 Status = tk.Label(ld_window, text="Hello!! :D", fg="blue", font=("Calibri", 9), bd=1, relief=tk.SUNKEN, anchor=tk.W, padx=3)
 Status.pack(side=tk.BOTTOM, fill=tk.X)
 
-ld_window.mainloop()
+ld_window.mainloop() 
