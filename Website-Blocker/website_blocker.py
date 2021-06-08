2b17ba41e7a72d8e91356e156d4078c8aba6e577
unknown <aanshipatwari@gmail.com>
2021-06-08 09:17:11 +0530

    changes

--- Website-Blocker/website_blocker.py
+++ Website-Blocker/website_blocker.py
@@ -1,39 +1,55 @@
-# importing the required libraries
+import time
+import platform
 from datetime import datetime as dt
 
-# Change the list to the websites you wish to block
-websites = [ "www.youtube.com", "twitter.com","www.skype.com/en", "www.facebook.com","www.instagram.com"]
-# Change the path to your hosts files folder
-host = "C:\Windows\System32\drivers\etc\hosts"
-# change the path to where you want to redirect 
-redirect = "127.0.0.1:8080"
+# Checking if the os is Windows, Mac Or Linux
+if 'windows' in platform.system().lower():
+    # Change the path to your hosts file path
+    hosts_path = "C:\\Windows\\System32\\drivers\\etc\\hosts"
+else:
+    hosts_path = "/etc/hosts"
 
-# Change the date when you want to start and end
-start = dt(2021, 4, 25)
-end = dt(2021, 4, 24)
+redirect_ip = "127.0.0.1"
 
+website_list = []
+
+# Input of the hours for blocking websites
+print("Enter the hour time(24hr format, Ex.16):")
+start_time = int(input("Enter the starting time:"))
+end_time = int(input("Enter the endin time: "))
+
+# Input of websites
+print("Enter the names of the website you want to block. Write q to stop")
 while True:
-    # When we wish to add the list of sites to be blocked
-    if start <= dt(dt.now().year, dt.now().month, dt.now().day) < end:
-        with open(host, "r+") as file:
-            temp = file.read()
-            for site in websites:
-                if site in temp:
+    a = input("Enter the url: ")
+    if a.lower() == 'q':
+        break
+    else:
+        if 'www.' in a:
+            website_list.append(a)
+            a = a.replace('www.', '')
+            website_list.append(a)
+        else:
+            website_list.append(a)
+            a = 'www.' + a
+            website_list.append(a)
+
+# Main Loop of the program
+while True:
+    if (dt(dt.now().year, dt.now().month, dt.now().day, start_time) < dt.now()) and (dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, end_time)):
+        with open(hosts_path, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
                     pass
                 else:
-                    # write command adds the sites to be blocked in the host file
-                    file.write(redirect + " " + site + "\n")
-        print("The site has been blocked.")
-        break
-    # When we wish to remove the list of sites to be blocked
+                    file.write(f"{redirect_ip} {website}\n")
     else:
-        with open(host, "r+") as file:
-            temp = file.readlines()
+        with open(hosts_path, 'r+') as file:
+            content = file.readlines()
             file.seek(0)
-            for line in temp:
-                if not any(site in line for site in websites):
+            for line in content:
+                if not any(website in line for website in website_list):
                     file.write(line)
-            # truncate command deletes the sites to be blocked from host file
             file.truncate()
-        print("The site has been unblocked.")
-        break
+    time.sleep(6)
