17f8b17dc638b514f3732d7f2d961c5c5371503e
Messaoud KIHAL <66731601+MesutKihal@users.noreply.github.com>
2022-09-13 22:17:03 +0100

    Update age_calc_gui.py

--- Age-Calculator-GUI/age_calc_gui.py
+++ Age-Calculator-GUI/age_calc_gui.py
@@ -1,63 +1,70 @@
 # import libraries
-from tkinter import *
+import tkinter as tk
 from datetime import date
 
-# initialized window
-root = Tk()
-root.geometry('280x300')
-root.resizable(0, 0)
-root.title('Age Calculator')
-statement = Label(root)
+# GUI App class
+class App:
+    def __init__(self):
+        # initialized window
+        self.master = tk.Tk()
+        self.master.geometry('280x300')
+        self.master.configure(bg="lightblue")
+        self.master.resizable(0, 0)
+        self.master.title('Age Calculator')
+        self.statement = tk.Label(self.master)
 
-# defining the function for calculating age
+    def run(self):
+        # creating a label for person's name to display
+        self.l1 = tk.Label(text="Name: ", font="courier 10", bg="lightblue")
+        self.l1.grid(row=1, column=0)
+        nameValue = tk.StringVar()
 
+        # creating a entry box for input
+        self.nameEntry = tk.Entry(self.master, textvariable=nameValue, relief="solid")
+        self.nameEntry.grid(row=1, column=1, padx=10, pady=10)
 
-def ageCalc():
-    global statement
-    statement.destroy()
-    today = date.today()
-    birthDate = date(int(yearEntry.get()), int(
-        monthEntry.get()), int(dayEntry.get()))
-    age = today.year - birthDate.year
-    if today.month < birthDate.month or today.month == birthDate.month and today.day < birthDate.day:
-        age -= 1
-    statement = Label(text=f"{nameValue.get()}'s age is {age}.")
-    statement.grid(row=6, column=1, pady=15)
+        # label for year in which user was born
+        self.l2 = tk.Label(text="Year: ", font="courier 10", bg="lightblue")
+        self.l2.grid(row=2, column=0)
+        yearValue = tk.StringVar()
+        self.yearEntry = tk.Entry(self.master, textvariable=yearValue, relief="solid")
+        self.yearEntry.grid(row=2, column=1, padx=10, pady=10)
 
+        # label for month in which user was born
+        self.l3 = tk.Label(text="Month: ", font="courier 10", bg="lightblue")
+        self.l3.grid(row=3, column=0)
+        monthValue = tk.StringVar()
+        self.monthEntry = tk.Entry(self.master, textvariable=monthValue, relief="solid")
+        self.monthEntry.grid(row=3, column=1, padx=10, pady=10)
 
-# creating a label for person's name to display
-l1 = Label(text="Name: ")
-l1.grid(row=1, column=0)
-nameValue = StringVar()
+        # label for day/date on which user was born
+        self.l4 = tk.Label(text="Day: ", font="courier 10", bg="lightblue")
+        self.l4.grid(row=4, column=0)
+        dayValue = tk.StringVar()
+        self.dayEntry = tk.Entry(self.master, textvariable=dayValue, relief="solid")
+        self.dayEntry.grid(row=4, column=1, padx=10, pady=10)
 
-# creating a entry box for input
-nameEntry = Entry(root, textvariable=nameValue)
-nameEntry.grid(row=1, column=1, padx=10, pady=10)
+        # defining the function for calculating age
+        def ageCalc():
+            self.statement.destroy()
+            today = date.today()
+            birthDate = date(int(self.yearEntry.get()), int(
+                self.monthEntry.get()), int(self.dayEntry.get()))
+            age = today.year - birthDate.year
+            if today.month < birthDate.month or today.month == birthDate.month and today.day < birthDate.day:
+                age -= 1
+            self.statement = tk.Label(text=f"{nameValue.get()}'s age is {age}.", font="courier 10", bg="lightblue")
+            self.statement.grid(row=6, column=1, pady=15)
 
-# label for year in which user was born
-l2 = Label(text="Year: ")
-l2.grid(row=2, column=0)
-yearValue = StringVar()
-yearEntry = Entry(root, textvariable=yearValue)
-yearEntry.grid(row=2, column=1, padx=10, pady=10)
+        # create a button for calculating age
+        self.button = tk.Button(text="Calculate age", font="courier 12 bold", fg="white", bg="dodgerblue", command=ageCalc)
+        self.button.grid(row=5, column=1)
+        
+        # infinite loop to run program
+        self.master.mainloop()
+        
 
-# label for month in which user was born
-l3 = Label(text="Month: ")
-l3.grid(row=3, column=0)
-monthValue = StringVar()
-monthEntry = Entry(root, textvariable=monthValue)
-monthEntry.grid(row=3, column=1, padx=10, pady=10)
-
-# label for day/date on which user was born
-l4 = Label(text="Day: ")
-l4.grid(row=4, column=0)
-dayValue = StringVar()
-dayEntry = Entry(root, textvariable=dayValue)
-dayEntry.grid(row=4, column=1, padx=10, pady=10)
-
-# create a button for calculating age
-button = Button(text="Calculate age", command=ageCalc)
-button.grid(row=5, column=1)
-
-# infinite loop to run program
-root.mainloop()
+if __name__ == '__main__':
+    age_calc = App()
+    age_calc.run()
+    
