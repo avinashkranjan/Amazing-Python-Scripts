65b6ef9b772002472b9dab3ec56a24f568b68dd6
Preeti Mahto <preetimahto6@gmail.com>
2023-06-07 06:13:20 +0530

    Added a Coffee Machine Game in Python

--- Coffee-Machine-py-game/coffee-machine-py-game.py
+++ Coffee-Machine-py-game/coffee-machine-py-game.py
@@ -1,5 +1,6 @@
 class CoffeeMachine:
     def __init__(self):
+        # Initialize the quantities of resources and money
         self.water = 500
         self.milk = 500
         self.coffee_beans = 200
@@ -7,6 +8,7 @@ class CoffeeMachine:
         self.money = 0
 
     def check_resources(self, water_needed, milk_needed, coffee_beans_needed, cups_needed):
+        # Check if there are enough resources to make the selected coffee
         if self.water < water_needed:
             return "Sorry, not enough water."
         elif self.milk < milk_needed:
@@ -20,6 +22,7 @@ class CoffeeMachine:
 
     def buy_coffee(self, choice):
         if choice == "espresso":
+            # Set the requirements and price for espresso
             water_needed = 50
             milk_needed = 0
             coffee_beans_needed = 18
@@ -27,6 +30,7 @@ class CoffeeMachine:
             price = 1.50
             coffee_type = "Espresso"
         elif choice == "latte":
+            # Set the requirements and price for latte
             water_needed = 200
             milk_needed = 150
             coffee_beans_needed = 24
@@ -34,6 +38,7 @@ class CoffeeMachine:
             price = 2.50
             coffee_type = "Latte"
         elif choice == "cappuccino":
+            # Set the requirements and price for cappuccino
             water_needed = 250
             milk_needed = 100
             coffee_beans_needed = 24
@@ -41,11 +46,14 @@ class CoffeeMachine:
             price = 3.00
             coffee_type = "Cappuccino"
         else:
+            # Return an error message for invalid choices
             message = "Invalid choice. Please try again."
             return message
 
+        # Check if there are enough resources to make the selected coffee
         message = self.check_resources(water_needed, milk_needed, coffee_beans_needed, cups_needed)
         if message == "Enough resources. Enjoy your coffee!":
+            # Prompt for inserting coins and calculate the total amount
             print(f"Please insert coins for {coffee_type} (${price}):")
             quarters = int(input("How many quarters?: "))
             dimes = int(input("How many dimes?: "))
@@ -56,6 +64,7 @@ class CoffeeMachine:
             if total_amount < price:
                 return "Insufficient amount. Money refunded."
             else:
+                # Calculate the change, update machine's properties, and return success message
                 change = round(total_amount - price, 2)
                 self.money += price
                 self.water -= water_needed
@@ -64,10 +73,12 @@ class CoffeeMachine:
                 self.cups -= cups_needed
                 return f"Here is ${change} in change. Here is your {coffee_type}. Enjoy!"
 
+        # Return the error message if there are insufficient resources
         return message
 
 
 def main():
+    # Create an instance of the CoffeeMachine class
     coffee_machine = CoffeeMachine()
 
     while True:
