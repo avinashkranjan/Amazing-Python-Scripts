7d4d3ded7d039bf0f28d50ab668585f455f8588d
zikani <46520540+zikani@users.noreply.github.com>
2023-06-10 20:47:03 +0200

    Update BMI_Calculator.py
    added 0-18, underweight and interpretations for each bmi meaning

--- BMI-Calculator-GUI/BMI_Calculator.py
+++ BMI-Calculator-GUI/BMI_Calculator.py
@@ -6,15 +6,30 @@ root.resizable(0, 0)
 root.title("BMI CALCULATOR")
 
 
-def BMI_Cal():
+def calculate_bmi():
     Bheight = float(var2.get())
     Bweight = float(var1.get())
-    BMI = str('%.2f' % (Bweight / (Bheight * Bheight)))
-    labelBMIResult.config(text=BMI)
+    BMI = Bweight / (Bheight * Bheight)
+
+    for range_, (category, interpretation) in categories.items():
+        if range_[0] <= BMI <= range_[1]:
+            labelBMIResult.config(text=f"BMI: {BMI:.2f}\n\nCategory: {category}\n\nInterpretation: {interpretation}")
+            break
+    else:
+        labelBMIResult.config(text="BMI: N/A\n\nCategory: N/A\n\nInterpretation: N/A")
 
 
 var1 = DoubleVar()
 var2 = DoubleVar()
+
+categories = {
+    (0, 18.5): ("Underweight", "You are underweight."),
+    (18.5, 24.9): ("Normal Weight", "You have a normal."),
+    (25, 29.9): ("Overweight", "You are overweight."),
+    (30, 34.9): ("Obesity Level I", " Seek professional advice."),
+    (35, 39.9): ("Obesity Level II", "Seek professional advice."),
+    (40, float('inf')): ("Obesity Level III", " Seek immediate professional advice.")
+}
 
 Tops = Frame(root, width=1350, height=50, bd=8, relief="raise")
 Tops.pack(side=TOP)
@@ -27,7 +42,7 @@ f2.pack(side=RIGHT)
 
 f1a = Frame(f1, width=600, height=200, bd=20, relief="raise")
 f1a.pack(side=TOP)
-f1b = Frame(f1, width=600, height=600, bd=20, relief='raise')
+f1b = Frame(f1, width=600, height=300, bd=20, relief='raise')
 f1b.pack(side=TOP)
 
 label1Title = Label(Tops,
@@ -73,12 +88,14 @@ labelBMIResult = Label(f1b,
                        pady=16,
                        bd=16,
                        fg='#000000',
-                       font=('arial', 30, 'bold'),
+                       font=('arial', 14, 'bold'),  # Adjust the font size here
                        bg='sky blue',
                        relief='sunk',
                        width=34,
-                       height=1)
+                       height=4,
+                       justify='center')
 labelBMIResult.grid(row=2, column=0)
+
 
 labelBMITable = Label(f2, font=("arial", 20, 'bold'),
                       text='BMI Table').grid(row=0, column=0)
@@ -86,15 +103,12 @@ txtlabelBMITable = Text(f2,
                         height=12,
                         width=38,
                         bd=16,
-                        font=("arial", 12, 'bold'))
+                        font=("arial", 11, 'bold'))
 txtlabelBMITable.grid(row=1, column=0)
 
-txtlabelBMITable.insert(END, 'Meaning \t\t' + "BMI \n\n")
-txtlabelBMITable.insert(END, 'Normal weight \t\t' + "19-24 \n\n")
-txtlabelBMITable.insert(END, 'Overwight \t\t' + "25-29,9 \n\n")
-txtlabelBMITable.insert(END, 'Obesity level I \t\t' + "30-34, 9 \n\n")
-txtlabelBMITable.insert(END, 'Obesity level II \t\t' + "35-39, 9\n\n")
-txtlabelBMITable.insert(END, 'Obesity level III \t\t' + ">= 40\n\n")
+txtlabelBMITable.insert(END, 'Range \t\t' + 'Category \t\t' + 'Interpretation\n\n')
+for range_, (category, interpretation) in categories.items():
+    txtlabelBMITable.insert(END, f'{range_[0]}-{range_[1]} \t\t{category} \t\t{interpretation}\n')
 
 btnBMI = Button(f2,
                 text="Click to \nCheck Your \nBMI",
@@ -104,7 +118,7 @@ btnBMI = Button(f2,
                 width=21,
                 font=("arial", 20, 'bold'),
                 height=3,
-                command=BMI_Cal)
+                command=calculate_bmi)
 btnBMI.grid(row=2, column=0)
 
 root.mainloop()
