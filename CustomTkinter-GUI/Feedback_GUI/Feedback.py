a7c552410b73472c9d86093c4caa2e94fe07d2ac
Harsh <harshshrivastava554@gmail.com>
2023-06-21 14:12:40 +0530

    Feedback GUI linting

--- CustomTkinter-GUI/Feedback_GUI/Feedback.py
+++ CustomTkinter-GUI/Feedback_GUI/Feedback.py
@@ -1,12 +1,28 @@
-import tkinter as tk
-import tkinter.messagebox
-from tkinter import PhotoImage
-from tkinter import messagebox
-from tkinter.font import Font
+# pylint: disable=invalid-name
+
+"""
+This module is imported to create a modern GUI.
+"""
+
 import customtkinter
 
 
 class FeedBack(customtkinter.CTk):
+    """
+    A class representing a feedback GUI.
+
+    This class inherits from customtkinter.CTk and provides a graphical user interface
+    for collecting user feedback.
+
+    Attributes:
+        textbox (customtkinter.CTkTextbox): The text box widget for displaying instructions.
+        feedback (customtkinter.CTkEntry): The entry widget for collecting user feedback.
+        button (customtkinter.CTkButton): The button widget for submitting the feedback.
+
+    Methods:
+        __init__(): Initializes the FeedBack class.
+        button_callback(): Callback function for the button click event.
+    """
     def __init__(self):
         super().__init__()
 
@@ -19,20 +35,30 @@ class FeedBack(customtkinter.CTk):
 
         self.textbox = customtkinter.CTkTextbox(master=self)
         self.textbox.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
-        text="         Please enter your valuable Feedback  \n We are sorry you have to delete your account  \n                     Help us to improve\n"
+        text = (
+        "         Please enter your valuable Feedback  \n"
+        " We are sorry you have to delete your account  \n"
+        "                     Help us to improve\n"
+        )
         self.textbox.insert("1.0", text)
 
         self.feedback = customtkinter.CTkEntry(master=self, placeholder_text="FeedBack")
-        self.feedback.grid(row=1, column=0, padx=10, pady=5,sticky='ew')
-                
-        self.button = customtkinter.CTkButton(master=self, command=self.button_callback, text="Done")
-        self.button.grid(row=2, column=0, padx=10, pady=(0,20), sticky="ew") 
+        self.feedback.grid(row=1, column=0, padx=10, pady=5, sticky='ew')
+
+        self.button = customtkinter.CTkButton(master=self, command=self.button_callback,text="Done")
+        self.button.grid(row=2, column=0, padx=10, pady=(0, 20), sticky="ew")
 
     def button_callback(self):
+        """
+        Callback function for the button click event.
+
+        This function is triggered when the user clicks the "Done" button.
+        It retrieves the user feedback from the input field and writes it to a file.
+        """
         def writing_feedback():
-            fdbck = self.feedback.get() 
-            with open('CustomTkinter-GUI\\Feedback_GUI\\FeedbackGUI', 'w') as feedbck:
-                feedbck.write(str(fdbck))  
+            fdbck = self.feedback.get()
+            with open('CustomTkinter-GUI/Feedback_GUI/FeedbackGUI','w',encoding="utf-8") as feedbck:
+                feedbck.write(str(fdbck))
         writing_feedback()
         self.destroy()
 
