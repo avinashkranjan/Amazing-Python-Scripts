7eb0e9c2b1bbf54df5d3df863f2361e9a4066332
akshaykhoje <code.akshaykhoje@gmail.com>
2023-07-03 12:25:18 +0530

    Edge cases handleed

--- Move-Mouse/move.py
+++ Move-Mouse/move.py
@@ -1,23 +1,24 @@
 import pyautogui as pgui
 import time
 
-def travelScreen():
-    pgui.FAILSAFE = False
 
-    screenWidth, screenHeight = pgui.size()    # screen resolution 
-    print(f"screenWidth : {screenWidth}, screenHeight : {screenHeight}")
+def travelScreen(screenWidth, screenHeight):
+    while True:
+        pgui.moveTo(2, 2)
 
-    pgui.moveTo(2, 2)
-
-    initial_x, initial_y = pgui.position()
-    print(f"Initial x : {initial_x}, Initial y : {initial_y}")
+        initial_x, initial_y = pgui.position()
+        print(f"Initial x : {initial_x}, Initial y : {initial_y}")
 
 
-    for x in range(initial_x, screenWidth - 2):
-        for y in range(initial_y, screenHeight - 2):
-            print(pgui.position())
-            pgui.moveTo(x,y)
+        for x in range(initial_x, screenWidth - 2):
+            for y in range(initial_y, screenHeight - 2):
+                print(pgui.position())
+                pgui.moveTo(x,y)
+
 
 if __name__ == "__main__":
-    travelScreen()
+    pgui.FAILSAFE = False
+    screenWidth, screenHeight = pgui.size()    # screen resolution 
+    print(f"Resolution : {screenWidth, screenHeight}")
 
+    travelScreen(screenWidth, screenHeight)
