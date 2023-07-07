8cbdcc405a5f9743b38df4c57fb7fbf68d1a62c8
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2023-07-07 18:52:56 +0530

    Update doh-dig.py

--- DOH-dig/doh-dig.py
+++ DOH-dig/doh-dig.py
@@ -1,33 +1,15 @@
-
-import ipaddress
-import json
 import requests
-from docopt import docopt
 
+def doh_dns_lookup(domain, record_type):
+    url = f"https://cloudflare-dns.com/dns-query?name={domain}&type={record_type}"
+    headers = {
+        "Accept": "application/dns-json"
+    }
 
-def cloudflare_lookup(type, record):
-    url = f"https://1.1.1.1/dns-query?name={record}&type={type}"
-    headers = {'accept': 'application/dns-json'}
     response = requests.get(url, headers=headers)
-    j_data = json.loads(response.text)
-    return j_data.get('Answer', j_data.get('Question'))
-
-
-valid_types = {'A', 'MX', 'PTR', 'SRV', 'TXT', 'NS'}
-
-if __name__ == '__main__':
-    arguments = docopt(__doc__, version='doh-dig 0.1')
-    if arguments['type']:
-        t = arguments['<type>'].upper()
-        r = arguments['<record>'].lower()
-        if t not in valid_types:
-            exit('Invalid type')
-        x = cloudflare_lookup(t, r)
-        print(json.dumps(x))
-    elif arguments['ptr']:
-        ip = arguments['<ip>']
-        arpa = ipaddress.ip_address(ip).reverse_pointer
-        x = cloudflare_lookup('PTR', arpa)
-        print(json.dumps(x))
+    if response.status_code == 200:
+        return response.json()
     else:
-        print(arguments)
+        return None
+
+
