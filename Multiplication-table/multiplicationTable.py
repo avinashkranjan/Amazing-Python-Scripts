62afda128071f3dbced29bd75318347c4183548a
nashira_26 <32673772+nashira26@users.noreply.github.com>
2023-07-23 21:10:07 +0530

    Update multiplicationTable.py
    Fixed errors as per guidelines

--- Multiplication-table/multiplicationTable.py
+++ Multiplication-table/multiplicationTable.py
@@ -1,25 +1,28 @@
+# prints out a multiplication table for a given number
 def generate_table(number):
     if number <= 0:
         print("Please enter a positive number")
         return
-    
+    # print heading
     print("Multiplication table upto", number, "\n")
-
     print("    ", end="")
+    # print header row
     for i in range(1, number+1):
         print(f"{i:4}", end="")
-    print("\n" + " "*4 + "-"*(number*4))   
-              
+    print("\n" + " "*4 + "-"*(number*4))
+    # print table cells
     for i in range(1, number+1):
         print(f"{i:2} |", end="")
         for j in range(1, number+1):
             result = i*j
             print(f"{result:4}", end="")
         print()
-    
+
+
 if __name__ == "__main__":
     try:
         num = int(input("Enter a positive integer: "))
         generate_table(num)
     except ValueError:
         print("Invalid number. Please enter a positive integer.")
+        
