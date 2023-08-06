9b59e32050103e90adc3a397c51b252e059df775
Syed Md Mihan Chistie <42113027+Mihan786Chistie@users.noreply.github.com>
2023-08-06 13:09:31 +0000

    updated video.py

--- YouTube-Scraper/video.py
+++ YouTube-Scraper/video.py
@@ -4,13 +4,41 @@ import json
 
 
 class Video:
-    
+    """
+    Create an instance of `Video` class.
+    ```python
+    video = Video(video_url="video_url")
+    ```
+
+    | Methods         | Details                  |
+    | --------------- | ------------------------ |
+    | `.getDetails()` | Return the video details |
+    """
 
     def __init__(self, video_url):
         self.video_url = video_url
 
     def getDetails(self):
-        
+        """
+        Class - `Video`
+        Example:
+        ```
+        video_data = Video("https://www.youtube.com/watch?v=pBy1zgt0XPc")
+        video_data.getDetails()
+        ```
+        Returns:
+        {
+            "title": Tile of the video
+            "description": Description of the video
+            "views_count": No. of views of the video,
+            "upload_date": Upload date of the video,
+            "comment_count": No. of comments,
+            "channel_name": Name of the channel,
+            "channel_avatar": Channel avatar,
+            "subscriber_count": No. of channel subscribers,
+            "channel_url": Link to the channel
+        }
+        """
         url = self.video_url
         try:
             res = requests.get(url)
