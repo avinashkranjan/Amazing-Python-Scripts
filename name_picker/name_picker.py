7279353f4b3ce14916d1e6e3e02244aed1130f18
AshirMehmood <ashwebtutorials@gmail.com>
2023-08-10 23:05:27 +0500

    added name_picker script

---
+++ name_picker/name_picker.py
@@ -0,0 +1,17 @@
+import random
+string_input = input(" Enter names or random strings ! separated by space")
+name_list = string_input.split()
+
+# the random name picker method
+def random_name_picker(char_list):
+    random.shuffle(char_list)
+    shuffled_list = char_list
+    length = len(shuffled_list)
+    random_index = random.randint(1, length)
+    new_name = ""
+    for x in range(random_index):
+        new_name =  new_name + (shuffled_list[x])
+    print(new_name)    
+    return new_name
+
+random_name_picker(name_list)    
