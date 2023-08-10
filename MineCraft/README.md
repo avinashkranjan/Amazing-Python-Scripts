26c31c59cc2cc6ca0743b88b822006bf6b9e2536
Killer2OP <prathammahajan2709@gmail.com>
2023-08-10 23:27:31 +0530

    [ADDED]: Minecraft using Python

---
+++ MineCraft/README.md
@@ -0,0 +1,45 @@
+# Minecraft-like 3D Environment with Ursina
+
+This project creates a simplified Minecraft-like 3D environment using the Ursina game engine. It allows players to interact with blocks, place and destroy them, and explore a basic virtual world.
+
+## Getting Started
+
+To run this project, you need to have Python and the Ursina library installed on your system. Follow these steps:
+
+1. Clone or download this repository to your local machine.
+2. Open a terminal/command prompt and navigate to the project directory.
+3. Run the following command to install the required dependencies:
+```
+pip install ursina
+```
+4. Run the `minecraft.py` script using the following command:
+```
+python minecraft.py
+```
+5. Enjoy the interactive Minecraft-like environment!
+
+## Controls
+
+- Move: W, A, S, D
+- Look Around: Mouse movement
+- Place Block: Left Mouse Button
+- Destroy Block: Right Mouse Button
+- Select Block Type:
+- Key 1: Grass Block
+- Key 2: Stone Block
+- Key 3: Brick Block
+- Key 4: Dirt Block
+
+## Features
+
+- First-person controller for player movement.
+- Placing and destroying blocks using mouse clicks.
+- Block types with different textures.
+- Skybox for an immersive environment.
+- Hand model with arm texture.
+- Clean and structured codebase.
+
+## Additional Notes
+
+- To close the game, you can use the standard window close button or keyboard shortcuts.
+- The FPS counter is disabled to provide a more immersive experience.
---
+++ MineCraft/minecraft.py
@@ -0,0 +1,87 @@
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+app = Ursina()
+grass_texture = load_texture('assets/grass_block.png')
+stone_texture = load_texture('assets/stone_block.png')
+brick_texture = load_texture('assets/brick_block.png')
+dirt_texture  = load_texture('assets/dirt_block.png')
+sky_texture   = load_texture('assets/skybox.png')
+arm_texture   = load_texture('assets/arm_texture.png')
+punch_sound   = Audio('assets/punch_sound',loop = False, autoplay = False)
+block_pick = 1
+
+window.fps_counter.enabled = False #To Disable the FPS Counter 
+window.exit_button.visible = False #To Remove the exit(close) Button
+
+def update():
+	global block_pick
+
+	if held_keys['left mouse'] or held_keys['right mouse']:
+		hand.active()
+	else:
+		hand.passive()
+
+	if held_keys['1']: block_pick = 1
+	if held_keys['2']: block_pick = 2
+	if held_keys['3']: block_pick = 3
+	if held_keys['4']: block_pick = 4
+
+class Voxel(Button):
+	def __init__(self, position = (0,0,0), texture = grass_texture):
+		super().__init__(
+			parent = scene,
+			position = position,
+			model = 'assets/block',
+			origin_y = 0.5,
+			texture = texture,
+			color = color.color(0,0,random.uniform(0.9,1)),
+			scale = 0.5)
+
+	def input(self,key):
+		if self.hovered:
+			if key == 'left mouse down':
+				punch_sound.play()
+				if block_pick == 1: voxel = Voxel(position = self.position + mouse.normal, texture = grass_texture)
+				if block_pick == 2: voxel = Voxel(position = self.position + mouse.normal, texture = stone_texture)
+				if block_pick == 3: voxel = Voxel(position = self.position + mouse.normal, texture = brick_texture)
+				if block_pick == 4: voxel = Voxel(position = self.position + mouse.normal, texture = dirt_texture)
+
+			if key == 'right mouse down':
+				punch_sound.play()
+				destroy(self)
+
+class Sky(Entity):
+	def __init__(self):
+		super().__init__(
+			parent = scene,
+			model = 'sphere',
+			texture = sky_texture,
+			scale = 150,
+			double_sided = True)
+
+class Hand(Entity):
+	def __init__(self):
+		super().__init__(
+			parent = camera.ui,
+			model = 'assets/arm',
+			texture = arm_texture,
+			scale = 0.2,
+			rotation = Vec3(150,-10,0),
+			position = Vec2(0.4,-0.6))
+
+	def active(self):
+		self.position = Vec2(0.3,-0.5)
+
+	def passive(self):
+		self.position = Vec2(0.4,-0.6)
+
+for z in range(20):
+	for x in range(20): 
+		voxel = Voxel(position = (x,0,z))
+
+player = FirstPersonController()
+sky = Sky()
+hand = Hand()
+
+app.run()
