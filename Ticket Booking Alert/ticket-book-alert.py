7351e9e324a21671222348618b18db3d001f18e6
Deeksha Jatangi D <79524527+DEEKSHACodeaim@users.noreply.github.com>
2023-08-11 09:45:20 +0530

    Update ticket-book-alert.py
    updated informal rule checks

--- Ticket Booking Alert/ticket-book-alert.py
+++ Ticket Booking Alert/ticket-book-alert.py
@@ -6,69 +6,72 @@ import re
 import smtplib
 import time
 
-url= "https://in.bookmyshow.com/buytickets/<movie_name>-<city>/movie-<city-code>-<refnumber>-MT/" #replace with the movie name, city, city code, mvie specific number
-#example of url :https://in.bookmyshow.com/buytickets/gadar-2-the-katha-continues-bengaluru/movie-bang-ET00338629-MT/20230811
-date= "<YYYYMMDD>" # eg: 20230101
-site= url+date
+url = "https://in.bookmyshow.com/buytickets/<movie_name>-<city>/movie-<city-code>-<refnumber>-MT/"  # replace with the movie name, city, city code, movie specific number
+'''example of URL
+ :https://in.bookmyshow.com/buytickets/gadar-2-the-katha-continues-bengaluru/movie-bang-ET00338629-MT/20230811'''
+date = "<YYYYMMDD>"  # eg: 20230101
+site = url + date
 hdr= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-       'Accept-Encoding': 'none',
-       'Accept-Language': 'en-US,en;q=0.8',
-       'Connection': 'keep-alive'}
-venue= 'CPCL' #this can be found by inspecting the element data-id for the venue where you would like to watch
-show= '9:30 PM' #replace it with your preferred show timing
-delay= 600 #timegap in seconds between 2 script runs
+      'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+      'Accept-Encoding': 'none',
+      'Accept-Language': 'en-US,en;q=0.8',
+      'Connection': 'keep-alive'}
+venue = 'CPCL'  # this can be found by inspecting the element data-id for the venue where you would like to watch
+show = '9:30 PM'  # replace it with your preferred show timing
+delay = 600   # timegap in seconds between 2 script runs
 
-to= 'abcd123@email.com' #relace with your email id on which you want to get the alert
-# Please add your username and password below, and make sure you 
-# toggle allow less secure apps to on 
+to = 'abcd123@email.com'  # relace with your email id on which you want to get the alert
+# Please add your username and password below, and make sure you
+# toggle allow less secure apps to on
 # https://myaccount.google.com/lesssecureapps?pli=1
-gmail_user= 'abcd123@xmail.com'
-gmail_passwd= 'abcd123'
-subject= 'Tickets are now available'
-text= 'Tickets are now available for '+show+' show at the venue'+venue
+gmail_user = 'abcd123@xmail.com'
+gmail_passwd = 'abcd123'
+subject = 'Tickets are now available'
+text = 'Tickets are now available for '+show+' show at the venue'+venue
+
 
 def send_mail():
+    '''Function to send mail'''
     print("Sending Email")
-    smtpserver= smtplib.SMTP("smtp.gmail.com",587)
+    smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
     smtpserver.ehlo()
     smtpserver.starttls()
-    smtpserver.login(gmail_user,gmail_passwd)
-    header= 'To:'+to+'\n'+'From:'+gmail_user
-    header= header+ '\n' + 'Subject:' + subject + '\n'
-    messg = header + '\n'+text+'\n\n'
-    smtpserver.sendmail(gmail_user,to,messg)
+    smtpserver.login(gmail_user, gmail_passwd)
+    header = 'To:' + to + '\n' + 'From:' + gmail_user
+    header = header + '\n' + 'Subject:' + subject + '\n'
+    messg = header + '\n' + text + '\n\n'
+    smtpserver.sendmail(gmail_user, to, messg)
     smtpserver.close()
 
-req= urllib.request.Request(site, headers=hdr)
+
+req = urllib.request.Request(site, headers = hdr)
 try:
-    #open the url
+    # open the url
     with urllib.request.urlopen(req) as page:
-        content= page.read()
-        soup= BeautifulSoup(content, 'html.parser')
-        soup2= soup.find_all('div',{'data-online': 'Y'})
-        line= str(soup2)
-        soup3= BeautifulSoup(line, 'html.parser')
-        # Find all 'a' elements with 'data-venue-code' attribute matching the given venue code
+        content = page.read()
+        soup = BeautifulSoup(content, 'html.parser')
+        soup2 = soup.find_all('div', {'data-online': 'Y'})
+        line = str(soup2)
+        soup3 = BeautifulSoup(line, 'html.parser')
+        # Find all 'a' elements with 'data-venue-code' attr
         soup4 = soup3.find_all('a', {'data-venue-code': venue})
         line1 = str(soup4)
         soup5 = BeautifulSoup(line1, 'html.parser')
-        # Find all 'a' elements with 'data-display-showtime' attribute matching the given showtime
+        # Find all 'a' elements with 'data-display-showtime' attr
         soup6 = soup3.find_all('a', {'data-display-showtime': show})
         line2 = str(soup6)
         # Use regular expression to find data-availability="A" in the modified HTML
         result = re.findall(r'data-availability="A"', line2)
         
-        if len(result)>0:
+        if len(result) > 0:
             print("Ticket Available")
             send_mail()
-        
+      
         else:
             print("Not available yet")
 
 except urllib.error.URLError as e:
-    print("Error occurred:",e)
+    print("Error occurred:", e)
 
 time.sleep(delay) #adjust the sleep timing by changing the delay variable as required
-
