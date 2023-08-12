546fd6ba789b8c3365889b9a6dbea54764269a01
Ruchita Patil <71245429+ruchspatil@users.noreply.github.com>
2023-08-12 14:20:57 +0530

    Update Random_Name_Picker.py

--- Random Name Picker/Random_Name_Picker.py
+++ Random Name Picker/Random_Name_Picker.py
@@ -2,32 +2,23 @@
 
 # The Python Script should:
 # Generate random names that will be included in the unique name.
-# Allow the user to enter name of their choice 
-
+# Allow the user to enter name of their choice
 # importing Libraries
 from random import randint
-
 # Welcome Statement
 print("\nWelcome to the Random Name Picker by Ruchita!")
-
 # input by the user
 input("Press a key to continue......")
-
 # Input how many names user want to use
 name_amount = int(input("\nInput how many names u want to use: "))
 names = []
 for x in range(name_amount):
     k = input("Enter name : \n")
     names.append(k)
-    
 # Random name picker
 random_number = randint(0, len(names))
-
 name_number = (random_number - 1)
-
 random_name = names[name_number]
-
 print("\nThe number of the name is : " + str(name_number) + "!!")
 print("\nRandomly selected name is: " + random_name + "!!" + " Congrats!")
-
 input("Press a key to continue......")
