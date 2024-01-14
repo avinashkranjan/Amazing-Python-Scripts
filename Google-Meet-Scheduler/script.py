a219cbd3c367a6428d8e2b2740ab4cb1187cb122
Vishnu Vardhan Vithanala <67997162+vishnu-v-vardhan@users.noreply.github.com>
2024-01-14 22:49:39 +0530

    Update script.py
    Update made to resolve the redefined variable issue presented by DeepSource.

--- Google-Meet-Scheduler/script.py
+++ Google-Meet-Scheduler/script.py
@@ -11,17 +11,17 @@ SCOPES = ["https://www.googleapis.com/auth/calendar"]
 class CreateMeet:
     def __init__(self, attendees: Dict[str, str], event_time: Dict[str, str], topic):
         authe = self._auth()
+        self.topic = topic  # Make topic a class attribute
         attendees_list = [{"email": e} for e in attendees.values()]
-        self.event_states = self._create_event(
-            attendees_list, event_time, authe, topic)
+        self.event_states = self._create_event(attendees_list, event_time, authe)
 
     @staticmethod
-    def _create_event(attendees: List[Dict[str, str]], event_time, authe: build, topic):
+    def _create_event(attendees: List[Dict[str, str]], event_time, authe: build):
         event_data = {"conferenceData": {"createRequest": {"requestId": f"{uuid4().hex}", "conferenceSolutionKey": {"type": "hangoutsMeet"}}},
                       "attendees": attendees,
                       "start": {"dateTime": event_time["start"], 'timeZone': 'Asia/Kolkata'},
                       "end": {"dateTime": event_time["end"], 'timeZone': 'Asia/Kolkata'},
-                      "summary": topic,
+                      "summary": CreateMeet.topic,  # Access topic as a class attribute
                       "reminders": {"useDefault": True}
                       }
         event = authe.events().insert(calendarId="primary", sendNotifications=True,
